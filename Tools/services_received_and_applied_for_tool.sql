8ff69fffddcedb2287238a6f04834899e6fcd569
justineschott <justine@educationsuperhighway.org>
2015-12-28 15:15:12 -0800

    Updating for Received/Applied for parameter

--- Tools/services_received_and_applied_for_tool.sql
+++ Tools/services_received_and_applied_for_tool.sql
@@ -36,7 +36,7 @@ lines_to_district_by_line_item as (
 ),
 /*Joins "line_items" table to add relevant cost, bandwidth, connection type, and purpose information; joins "districts" table
 to add demographic data used in metric calculations*/
-internet_services_received as (
+services_received as (
       select ldli.district_esh_id,
       d.name,
       d.postal_cd,
@@ -179,24 +179,137 @@ students (e.g. num_students in district/num_students in ALL districts served by 
       where li.broadband=true  
        and (li.consortium_shared=false 
          OR li.ia_conditions_met=true 
+         OR li.wan_conditions_met=true 
+         OR li.upstream_conditions_met=true 
+         OR 'backbone'=any(li.open_flags)
+       )  
+       and d.include_in_universe_of_districts=true 
+       and (case when 'exclude'=any(li.open_flags) then true else false end)=false
+),
+/*Joins "line_items" table to add relevant cost, bandwidth, connection type, and purpose information; joins "districts" table
+to add demographic data used in metric calculations*/
+services_applied_for as (
+      select d.esh_id as district_esh_id,
+      d.name,
+      d.postal_cd,
+      li.id as line_item_id,
+      li.consortium_shared,
+
+--classifies circuits as either "Shared IA", "Backbone", or "District-dedicated"
+      case when li.consortium_shared=true 
+            and li.ia_conditions_met=true
+              then 'Shared IA'
+           when 'backbone'=any(li.open_flags)
+              then 'Backbone'
+              else 'District-dedicated' 
+      end as shared_service,
+
+--not applicable at applicant level
+      'n/a' as quantity_of_lines_received_by_district,
+
+      li.bandwidth_in_mbps,
+      li.connect_type,
+      li.purpose,
+      li.wan,
+
+/*For "District-dedicated circuits", the district share of the line item cost is calculated by 
+multiplying the line item cost by the proportion of the total num_lines allocated to 
+the district (see previous comment).  For "Shared IA" and "Backbone" line items, line item cost is divided 
+proportionally between recipient districts based on their number of 
+students (e.g. num_students in district/num_students in ALL districts served by line item)*/ 
+
+      'n/a' as line_item_district_monthly_cost,
+
+--if months of service is null or zero, we assume that the services are in use throughout the entirety of the E-rate cycle
+      case when li.orig_r_months_of_service is not null 
+           and li.orig_r_months_of_service!=0 
+            then li.total_cost::numeric/li.orig_r_months_of_service 
+            else li.total_cost::numeric/12 
+      end as "line_item_total_monthly_cost",
+
+      'n/a' as "cat.1_allocations_to_district",
+      li.num_lines as "line_item_total_num_lines",
+      li.total_cost as "line_item_total_cost",
+      li.rec_elig_cost as "line_item_recurring_elig_cost",
+      li.one_time_eligible_cost as "line_item_one-time_cost",
+      li.orig_r_months_of_service,
+      li.applicant_name,
+      li.isp_conditions_met,
+      li.internet_conditions_met,
+      li.ia_conditions_met,
+      li.wan_conditions_met,
+      li.upstream_conditions_met,
+      li.broadband,
+      li.service_provider_name,
+      li.exclude,
+      array_to_string(li.open_flags,', ') as open_flags,
+      
+      case when 'exclude'=any(open_flags) then true else false end as dqs_excluded,
+      
+      case when d.ia_cost_per_mbps!='Insufficient data' 
+            and d.ia_cost_per_mbps!='Infinity' 
+            and d.ia_cost_per_mbps!='NaN' 
+              then d.ia_cost_per_mbps::numeric/12 
+              else null 
+      end as district_monthly_ia_cost_per_mbps,
+
+--total IA "backed out" from metrics by multiplying bandwidth per student by num_students
+      case when d.ia_bandwidth_per_student!='Insufficient data' 
+            and d.num_students!='No data' 
+              then ((d.ia_bandwidth_per_student::numeric*d.num_students::bigint)/1000)::int
+              else null 
+      end as district_total_ia_rounded_to_nearest_mbps,
+
+      d.ia_bandwidth_per_student,
+      d.num_students,
+      d.num_students_and_staff,
+      d.num_schools,
+      d.latitude,
+      d.longitude,
+      d.locale,
+      d.district_size,
+      d.exclude_from_analysis,
+      d.consortium_member
+
+      from districts d
+
+      left join line_items li
+      on d.esh_id=li.applicant_id
+
+      where li.broadband=true  
+       and (li.consortium_shared=false 
+         OR li.ia_conditions_met=true 
+         OR li.wan_conditions_met=true 
+         OR li.upstream_conditions_met=true 
          OR 'backbone'=any(li.open_flags)
        )  
        and d.include_in_universe_of_districts=true 
        and (case when 'exclude'=any(li.open_flags) then true else false end)=false
 )
-
   
 select * 
-from internet_services_received sr
+
+    {% if received_or_applied_for == 'received' %}
+from services_received svcs
+    {% endif %}
+    {% if received_or_applied_for == 'applied_for' %}
+from services_applied_for svcs
+    {% endif %}
 
 --Liquid parameters allow user to filter for district_esh_id, state, and clean status
-where (sr.district_esh_id::varchar='{{district_esh_id}}' OR 'All'='{{district_esh_id}}') 
-  and (sr.postal_cd='{{state}}' OR 'All'='{{state}}') 
-  and (sr.exclude_from_analysis::varchar='{{exclude_from_analysis}}' OR 'All'='{{exclude_from_analysis}}')
+where (svcs.district_esh_id::varchar='{{district_esh_id}}' OR 'All'='{{district_esh_id}}') 
+  and (svcs.postal_cd='{{state}}' OR 'All'='{{state}}') 
+  and (svcs.exclude_from_analysis::varchar='{{exclude_from_analysis}}' OR 'All'='{{exclude_from_analysis}}')
 
 ORDER BY district_esh_id
 
 {% form %}
+received_or_applied_for:
+  type: select
+  default: 'received'
+  options: [['received'],
+            ['applied_for']
+           ]
 
 district_esh_id:
   type: text
@@ -213,5 +326,4 @@ exclude_from_analysis:
             ['false'],
             ['All']
            ]
-
 {% endform %}
