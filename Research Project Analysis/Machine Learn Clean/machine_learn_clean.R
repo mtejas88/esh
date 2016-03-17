8da233dc4708423bc801042b3c98e7653f17bf40
justineschott <justine@educationsuperhighway.org>
2016-03-16 17:11:10 -0700

    Comparing multivariate to single
    not yet able to save outputs but the code works @wardch

--- Research Project Analysis/Machine Learn Clean/machine_learn_clean.R
+++ Research Project Analysis/Machine Learn Clean/machine_learn_clean.R
@@ -22,18 +22,55 @@ class(joined$rec_elig_cost.y)
 joined$rec_elig_cost.y <- as.numeric(as.character(joined$rec_elig_cost.y))
 joined <- joined[!is.na(joined$rec_elig_cost.y),]
 
+mini_joined <- joined[,c(5,8,10,11,12,13,15,18,19,26,36,38)]
+head(mini_joined)
+
 nrow(orig)
 nrow(verified)
 nrow(joined)
 
 ### Validation Set ###
-data.size <- nrow(joined)
+data.size <- nrow(mini_joined)
 train.size <- round(data.size *.8, 0)
 dev.size <- data.size - train.size
-train_vector <- sample(data.size, train.size)
 
-##sample random forest###
+train_data <- mini_joined[1:train.size,]
+head(train_data)
+nrow(train_data)
+
+##multivariate random forest###
+bw_connect.mreg <- rfsrc(Multivar(bandwidth_in_mbps.y, connect_category.y) ~., data = train_data)
+print(bw_connect.mreg, outcome.target = "bandwidth_in_mbps.y")
+print(bw_connect.mreg, outcome.target = "connect_category.y")
+plot(bw_connect.mreg, outcome.target = "bandwidth_in_mbps.y")
+plot(bw_connect.mreg, outcome.target = "connect_category.y")
+
+##singular random forest###
+train_data.bw <- train_data[,c(1:11)]
+train_data.connect <- train_data[,c(1:10,12)]
+
+bw.reg <- rfsrc(bandwidth_in_mbps.y ~., data = train_data.bw)
+connect.reg <- rfsrc(connect_category.y ~., data = train_data.connect)
+print(bw.reg)
+print(connect.reg)
+plot(bw.reg)
+plot(connect.reg)
+
+##singular random forest, diff package###
+bw.reg_rf <- randomForest(bandwidth_in_mbps.y ~ ., 
+                               data=train_data.bw, importance=T, ntree=501)
+connect.reg_rf <- randomForest(connect_category.y ~ ., 
+                          data=train_data.connect, importance=T, ntree=501)
+print(bw.reg_rf)
+print(connect.reg_rf)
+plot(bw.reg_rf)
+plot(connect.reg_rf)
+
+###example multivariate regression###
+
 mtcars.mreg <- rfsrc(Multivar(mpg, cyl) ~., data = mtcars)
 print(mtcars.mreg, outcome.target = "mpg")
-head(mtcars)
-head(orig)
+print(mtcars.mreg, outcome.target = "cyl")
+plot(mtcars.mreg, outcome.target = "mpg")
+plot(mtcars.mreg, outcome.target = "cyl")
+
