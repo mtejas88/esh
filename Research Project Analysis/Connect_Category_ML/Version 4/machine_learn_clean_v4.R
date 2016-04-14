d169de349118e3f2749ede71d33e6f4b3412f633
wardch <charlieward18@gmail.com>
2016-04-14 14:41:48 -0700

    added inital work for montana and new mexico

--- Research Project Analysis/Connect_Category_ML/Version 4/machine_learn_clean_v4.R
+++ Research Project Analysis/Connect_Category_ML/Version 4/machine_learn_clean_v4.R
@@ -15,44 +15,39 @@ library(randomForestSRC)
 # require("RPostgreSQL")
 
 
-# loads the PostgreSQL driver
-
+### Avg on 5 runs: 91.36% ###
 
 
 setwd("~/Desktop/ESH/ficher/Research Project Analysis/Connect_Category_ML/Version 4")
 
-
-
 verified_lis <- read.csv("~/Desktop/ESH/ficher/Research Project Analysis/Connect_Category_ML/Version 4/verified_line_items_v4.csv")
-
 raw_lis <- read.csv("~/Desktop/ESH/ficher/Research Project Analysis/Connect_Category_ML/Version 4/original_line_items_v4.csv")
 
-nrow(verified_lis)
-summary(verified_lis)
-# View(verified_lis)
 
 # left join both datasets
 joined = merge(x = raw_lis, y = verified_lis, by = 'frn_complete', all.x = TRUE)
 
+compacted_joined <- joined[, c('connect_category.y', 'connect_category.x', 'frn_complete', 'bandwidth_in_mbps.x',
+                               'consultant_app.x', 'cost_per_line.x', 'num_students.x',
+                               'num_of_services.x', 'highest_connect_type.x', 'providers_typical_category.x',
+                               'free_and_reduced.x', 'copper_line.x', 'consultants_cat.x',
+                               'upstream_conditions_met.x', 'isp_conditions_met.x', 'ia_circuit_only.x',
+                               'wireless_service.x', 'wan_fiber_3_lines.x', 'likely_wan_fiber.x', 'exception_not_fiber.x',
+                               'likely_other_uncategorized.x', 'entity_type.x', 'other_location_no_district.x')]
 
-compacted_joined <- joined[, c('connect_category.y', 'connect_category.x', 'frn_complete', 'bandwidth_in_mbps.x', 'consultant_app', 'consultant_app', 'cost_per_line', 'num_students.x', 'num_of_services',
-                               'highest_connect_type', 'providers_typical_category', 'free_and_reduced', 'copper_line', 'consultants_cat', 
-                               'upstream_conditions_met.x', 'isp_conditions_met.x', 'ia_circuit_only', 
-                               'wireless_service', 'wan_fiber_3_lines', 'likely_wan_fiber', 'exception_not_fiber', 'likely_other_uncategorized')]
 
+# Begin filtering out nulls. (filtering our NA's could introduce bias too,
+# so we want to be careful here about how we handle missing values.)
 
 # Begin filtering out nulls. (filtering our NA's could introduce bias too, 
 # so we want to be careful here about how we handle missing values.)
 # View(compacted_joined)
-nrow(compacted_joined)
- 
-summary(compacted_joined)
-
 
 compacted_joined <- na.omit(compacted_joined)
+compacted_joined <- compacted_joined[compacted_joined$other_location_no_district.x != 'true',]
+summary(compacted_joined)
 
-
-nrow(compacted_joined) # 6519 Observations before splitting
+nrow(compacted_joined) # 8889 Observations before splitting
 
 ### Translate into current ML work ###
 
@@ -67,17 +62,19 @@ training_id_integers <- sample(data.size, train.size)
 
 train_data <- compacted_joined[training_id_integers,]
 test_data <- compacted_joined[-training_id_integers,]
-nrow(train_data) # 5215 to train with
-nrow(test_data) # 1304 Observations for testing
-# View(train_data)
+nrow(train_data) # 7111 to train with
+nrow(test_data) # 1778 Observations for testing
+
+summary(test_data)
 
 ### Creating Random Forest Model, with 18 variables ###
-connect.forest <- randomForest(as.factor(connect_category.y) ~ bandwidth_in_mbps.x + consultant_app + consultants_cat
-                               + cost_per_line + num_students.x + num_of_services + highest_connect_type 
-                               + providers_typical_category + free_and_reduced + copper_line + upstream_conditions_met.x
-                               + isp_conditions_met.x + ia_circuit_only + wireless_service + wan_fiber_3_lines
-                               + likely_wan_fiber + exception_not_fiber + likely_other_uncategorized,
-                                data=train_data, importance=T, ntree=501)
+connect.forest <- randomForest(as.factor(connect_category.y) ~ bandwidth_in_mbps.x + consultant_app.x + consultants_cat.x
+                               + cost_per_line.x + num_students.x + num_of_services.x + highest_connect_type.x
+                               + providers_typical_category.x + free_and_reduced.x + copper_line.x
+                               + isp_conditions_met.x + ia_circuit_only.x + wireless_service.x + wan_fiber_3_lines.x
+                                + exception_not_fiber.x + likely_other_uncategorized.x + entity_type.x,
+                               data=train_data, importance=T, ntree=501)
+
 
 connect.forest$importance
 varImpPlot(connect.forest)
@@ -90,7 +87,79 @@ results$guessed_correctly <- results$verified_category == results$predicted_cate
 
 # clean up factors issue
 
-View(results)
+#View(results)
 confusionMatrix(predict.forest, reference=test_data$connect_category.y)
+
+
+##### NOW FOR NM AND MO #####
+
+mo_nm_crimson <- read.csv("~/Desktop/ESH/ficher/Research Project Analysis/Connect_Category_ML/Version 4/mo_nm_crimson.csv")
+
+# mo_nm_onyx <- read.csv("~/Desktop/ESH/ficher/Research Project Analysis/Connect_Category_ML/Version 4/mo_nm_onyx.csv")
+
+
+verified_lis <- read.csv("~/Desktop/ESH/ficher/Research Project Analysis/Connect_Category_ML/Version 4/verified_line_items_v4.csv")
+summary(verified_lis)
+
+
+compacted_joined <- verified_lis[, c('connect_category', 'frn_complete', 'bandwidth_in_mbps',
+                               'consultant_app', 'cost_per_line', 'num_students',
+                               'num_of_services', 'highest_connect_type', 'providers_typical_category',
+                               'free_and_reduced', 'copper_line', 'consultants_cat',
+                               'upstream_conditions_met', 'isp_conditions_met', 'ia_circuit_only',
+                               'wireless_service', 'wan_fiber_3_lines', 'likely_wan_fiber', 'exception_not_fiber',
+                               'likely_other_uncategorized', 'entity_type', 'other_location_no_district')]
+
+mo_nm_crimson <- mo_nm_crimson[, c('connect_category', 'frn_complete', 'bandwidth_in_mbps',
+                                     'consultant_app', 'cost_per_line', 'num_students',
+                                     'num_of_services', 'highest_connect_type', 'providers_typical_category',
+                                     'free_and_reduced', 'copper_line', 'consultants_cat',
+                                     'upstream_conditions_met', 'isp_conditions_met', 'ia_circuit_only',
+                                     'wireless_service', 'wan_fiber_3_lines', 'likely_wan_fiber', 'exception_not_fiber',
+                                     'likely_other_uncategorized', 'entity_type', 'other_location_no_district')]
+
+
+
+# Begin filtering out nulls. (filtering our NA's could introduce bias too,
+# so we want to be careful here about how we handle missing values.)
+
+# Begin filtering out nulls. (filtering our NA's could introduce bias too, 
+# so we want to be careful here about how we handle missing values.)
+# View(compacted_joined)
+
+compacted_joined <- na.omit(compacted_joined)
+compacted_joined <- compacted_joined[compacted_joined$other_location_no_district != 'true',]
+nrow(compacted_joined) # 8889 Observations before splitting
+summary(compacted_joined)
+
+### Creating Random Forest Model, with 18 variables ###
+connect.forest <- randomForest(as.factor(connect_category) ~ bandwidth_in_mbps + consultant_app + consultants_cat
+                               + cost_per_line + num_students + num_of_services + highest_connect_type
+                               + providers_typical_category + free_and_reduced + copper_line
+                               + isp_conditions_met + ia_circuit_only + wireless_service + wan_fiber_3_lines
+                               + exception_not_fiber + likely_other_uncategorized + entity_type,
+                               data=compacted_joined, importance=T, ntree=501)
+
+
+connect.forest$importance
+varImpPlot(connect.forest)
+
+
+summary(mo_nm_crimson)
+summary(compacted_joined)
+
+predict.forest <- predict(connect.forest, mo_nm_crimson)
+
+### Showing results:
+results <- data.frame(predicted_category = predict.forest, raw_category = mo_nm_crimson$connect_category, mo_nm_crimson$frn_complete)
+
+results$guessed_differently <- results$raw_category != results$predicted_category
+
+# clean up factors issue
+
+View(results)
+# confusionMatrix(predict.forest, reference=test_data$connect_category.y)
+
+
 
 
