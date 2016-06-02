aa9e13189198adc96768a58b406128920679cae1
justineschott <justine@educationsuperhighway.org>
2016-06-02 09:57:10 -0800

    Add 2015 suffix and table prefixes
    to tell difference between 2015, 2106, etc... data tables

--- Views/Standard/line_item_district_association_2015.sql
+++ Views/Standard/line_item_district_association_2015.sql
@@ -1,13 +1,13 @@
 select dl.district_esh_id,
 	   a.line_item_id
-from allocations a
-join district_lookup_nifs dl
+from public.allocations a
+join district_lookup_nifs_2015 dl
 on a.recipient_id = dl.esh_id
 
 /*
 Author:                       Justine Schott
 Created On Date:              03/03/2016
-Last Modified Date: 
+Last Modified Date: 		  06/02/2016
 Name of QAing Analyst(s):  
 Purpose:                      To determine all possible line items received by a district for the purpose of aiding data cleaning
 */
