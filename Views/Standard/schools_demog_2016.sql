7b07705eff4a7507af0f1db18e840329119ee213
Justine <justine@educationsuperhighway.org>
2016-06-16 18:23:59 -0700

    Update
    include esh_ids

--- Views/Standard/schools_demog_2016.sql
+++ Views/Standard/schools_demog_2016.sql
@@ -1,4 +1,9 @@
- select sc131a."LEAID" as nces_cd,
+ select d.esh_id as district_esh_id,
+        case
+          when eim.entity_id is null then 'Unknown for 2015'
+            else eim.entity_id::varchar
+        end as school_esh_id,
+        sc131a."LEAID" as nces_cd,
         sc131a."NCESSCH" as school_nces_code,
         case when sc131a."TYPE" = '1' then 1 end as school_type_1_indicator,
         case
@@ -14,6 +19,9 @@ join (select *
       from districts_demog_2016
       where postal_cd not in ('MT', 'VT')) d --only want schools in districts universe
 on ag131a."LEAID" = d.nces_cd
+left join ( select distinct entity_id, nces_code
+            from public.entity_nces_codes) eim
+on sc131a."NCESSCH" = eim.nces_code
 where sc131a."GSHI" != 'PK' 
 and sc131a."STATUS" != '2' --closed schools
 and sc131a."VIRTUALSTAT" != 'VIRTUALYES'
@@ -21,7 +29,12 @@ and sc131a."TYPE" in ('1','2','3','4') --research is still outstanding to determ
 
 UNION
 
-select  d.nces_cd,
+select  d.esh_id as district_esh_id,
+        case
+          when eim.entity_id is null then 'Unknown for 2015'
+            else eim.entity_id::varchar
+        end as school_esh_id,
+        d.nces_cd,
         sc131a."NCESSCH" as school_nces_code,
         case when sc131a."TYPE" = '1' then 1 end as school_type_1_indicator,
         case
@@ -39,6 +52,9 @@ join (select *
       from districts_demog_2016
       where postal_cd = 'MT') d --only want schools in districts universe
 on ag131a."LSTREE" = d.address
+left join ( select distinct entity_id, nces_code
+            from public.entity_nces_codes) eim
+on sc131a."NCESSCH" = eim.nces_code
 where sc131a."GSHI" != 'PK' 
 and sc131a."STATUS" != '2' --closed schools
 and sc131a."VIRTUALSTAT" != 'VIRTUALYES'
@@ -46,7 +62,12 @@ and sc131a."TYPE" in ('1','2','3','4') --research is still outstanding to determ
 
 UNION
 
-select  d.nces_cd,
+select  d.esh_id as district_esh_id,
+        case
+          when eim.entity_id is null then 'Unknown for 2015'
+            else eim.entity_id::varchar
+        end as school_esh_id,
+        d.nces_cd,
         sc131a."NCESSCH" as school_nces_code,
         case when sc131a."TYPE" = '1' then 1 end as school_type_1_indicator,
         case
@@ -62,6 +83,9 @@ join (select *
       from districts_demog_2016
       where postal_cd = 'VT') d
 on sc131a."UNION" = d.union_code
+left join ( select distinct entity_id, nces_code
+            from public.entity_nces_codes) eim
+on sc131a."NCESSCH" = eim.nces_code
 where sc131a."GSHI" != 'PK' 
 and sc131a."STATUS" != '2' --closed schools
 and sc131a."VIRTUALSTAT" != 'VIRTUALYES'
