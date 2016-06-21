bb72c7fef1a05d3965782cf269641544fd87c98b
justineschott <justine@educationsuperhighway.org>
2016-06-20 19:53:54 -0800

    Updates to run

--- Views/Standard/school_aggregation_2016.sql
+++ Views/Standard/school_aggregation_2016.sql
@@ -1,30 +1,30 @@
-select 	s.esh_id,
+select 	s.school_esh_id,
        	count(distinct 	case
-													when	connect_category ilike '%fiber%'
-													and isp_conditions_met = false								
-													and	num_open_flags	=	0
-													and consortium_shared = false
-													and backbone_conditions_met = false	
-														then s.esh_id
+													when	li.connect_category ilike '%fiber%'
+													and li.isp_conditions_met = false								
+													and	li.num_open_flags	=	0
+													and li.consortium_shared = false
+													and li.backbone_conditions_met = false	
+														then s.school_esh_id
 												end) as fiber_wan_indicator,
        	count(distinct 	case
-													when	connect_category ilike '%fiber%'
-													and (internet_conditions_met = true or upstream_conditions_met = true)								
-													and	num_open_flags	=	0
-													and consortium_shared = false
-													and backbone_conditions_met = false	
-														then s.esh_id
+													when	li.connect_category ilike '%fiber%'
+													and (li.internet_conditions_met = true or li.upstream_conditions_met = true)								
+													and	li.num_open_flags	=	0
+													and li.consortium_shared = false
+													and li.backbone_conditions_met = false	
+														then s.school_esh_id
 												end) as fiber_internet_upstream_indicator
         
 from fy2016.entity_circuits ec
 join fy2016.circuits c
 on ec.circuit_id = c.id
-join fy2016.line_items
-on c.line_item_id = line_items.id
-right join schools s
-on ec.entity_id::varchar = s.esh_id
+join fy2016.line_items li
+on c.line_item_id = li.id
+right join schools_demog_2016 s
+on ec.entity_id::varchar = s.school_esh_id
 
-group by  d.esh_id
+group by  s.school_esh_id
 
 /*
 Author:                       Justine Schott
