6166d0ae852d4a9c97026252e3a5a61bfff2d612
justineschott <justine@educationsuperhighway.org>
2016-09-02 13:37:27 -0800

    Stage Indicator updates
    brad QA:
    if you don't receive a service, go to target
    if you're 1 campus with fiber IA, go to closed lost
    update num_lines / recipients / allocations safeguard to look at
    district level instead of line item level

--- Tools/stage_indicator.sql
+++ Tools/stage_indicator.sql
@@ -1,9 +1,11 @@
 select 
 	d.esh_id,
-	case when non_fiber_lines_w_dirty > 0 then																			--row 1
+	case when lines_w_dirty = 0 then 'FIBER TARGET'																	--per brad, dqs cleaning standards
+	when num_campuses = 1 and fiber_internet_upstream_lines_w_dirty > 0 then 'CLOSED LOST'							--per brad, dqs cleaning standards
+	when non_fiber_lines_w_dirty > 0 then																			--row 1
 		case when num_campuses = 1 and fiber_internet_upstream_lines_w_dirty > 0 then									--row 2
 			case when 	exclude_from_analysis = false then																--row 3
-				'CLOSED LOST' else 'dont update' end 																	--rows 4,5
+				'CLOSED LOST 3/4/5' else 'dont update 3/4/5' end 														--rows 4,5
 		else	
 			case when district_specif_recip_nonfiber_lines > 0 and campuses_specif_recip_nonfiber_lines = 0 then		--row 6
 					case when 	non_fiber_lines_w_dirty > 0 then														--row 7
@@ -12,16 +14,16 @@ select
 								case 	when 	exclude_from_analysis = false and 
 												priority_status__c not in ('Priority 1','Priority 3') and
 												(not(array_to_string(flag_array,',') ilike '%wan%')
-												or flag_array is null) then 'CLOSED LOST'
+												or flag_array is null) then 'CLOSED LOST 11/12'
 										when 	exclude_from_analysis = true and 
 												priority_status__c not in ('Priority 1','Priority 3') and
-												array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL'			--row 10
-								 		else 'dont update' end															--rows 11,12
+												array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL 11/12'	--row 10
+								 		else 'dont update 11/12' end													--rows 11,12
 							else 
 								case when 	district_recip_clean_nonfiber_lines > 0 and 
 											(not('district_missing_ia' = any(flag_array)) or flag_array is null) and
 											(not('district_isp_missing_upstream' = any(flag_array)) or flag_array is null) then
-									'FIBER TARGET' else 'dont update' end	
+									'FIBER TARGET 13/14/15' else 'dont update 13/14/15' end	
 							end																							--rows 13,14,15
 						else 
 							case when 	num_campuses = campuses_specif_recip_fiber_internet_upstream_lines and
@@ -29,60 +31,64 @@ select
 								case 	when 	exclude_from_analysis = false and 
 												priority_status__c not in ('Priority 1','Priority 3') and
 												(not(array_to_string(flag_array,',') ilike '%wan%')
-												or flag_array is null) then 'CLOSED LOST'
+												or flag_array is null) then 'CLOSED LOST 18/19'
 										when 	exclude_from_analysis = true and 
 												priority_status__c not in ('Priority 1','Priority 3') and
-												array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL'			--row 17
-								 		else 'dont update' end															--rows 18,19
+												array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL 18/19'	--row 17
+								 		else 'dont update 18/19' end													--rows 18,19
 							else 
 								case when 	district_recip_clean_nonfiber_lines > 0 and 
 											(not('district_missing_ia' = any(flag_array)) or flag_array is null) and
 											(not('district_isp_missing_upstream' = any(flag_array)) or flag_array is null) then
-									'FIBER TARGET' else 'dont update' end	
+									'FIBER TARGET 20/21/22' else 'dont update 20/21/22' end	
 							end																							--rows 20,21,22
 						end
 					else
 						case 	when 	exclude_from_analysis = false and 
 										priority_status__c not in ('Priority 1','Priority 3') and
 										(not(array_to_string(flag_array,',') ilike '%wan%')
-										or flag_array is null) then 'CLOSED LOST'
+										or flag_array is null) then 'CLOSED LOST 24/25'
 								when 	exclude_from_analysis = true and 
 										priority_status__c not in ('Priority 1','Priority 3') and
-										array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL'					--row 23
-						 		else 'dont update' end																	--rows 24,25
+										array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL 24/25'			--row 23
+						 		else 'dont update 24/25' end															--rows 24,25
 					end
 			else
-				case when campuses_specif_recip_nonfiber_not_wan_lines_alloc = 0 then									--row 26
+				case when 	campuses_specif_recip_nonfiber_not_wan_lines_alloc = 0 or
+							fiber_internet_upstream_lines_w_dirty >= sum_alloc_ia_fiber_lines or 
+							fiber_internet_upstream_lines_w_dirty >= count_ben_ia_fiber_lines then						--row 26
 					case when fiber_internet_upstream_lines_w_dirty > 0 then 											--row 27
 						case 	when 	exclude_from_analysis = false and 
 										priority_status__c not in ('Priority 1','Priority 3') and
 										(not(array_to_string(flag_array,',') ilike '%wan%')
-										or flag_array is null) then 'CLOSED LOST'
+										or flag_array is null) then 'CLOSED LOST 29/30'
 								when 	exclude_from_analysis = true and 
 										priority_status__c not in ('Priority 1','Priority 3') and
-										array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL'					--row 28
-						 		else 'dont update' end																	--rows 29,30
+										array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL 29/30'			--row 28
+						 		else 'dont update 29/30' end															--rows 29,30
 					else 
 						case when 	district_recip_clean_nonfiber_lines > 0 and 
 									(not('district_missing_ia' = any(flag_array)) or flag_array is null) and
 									(not('district_isp_missing_upstream' = any(flag_array)) or flag_array is null) then
-							'FIBER TARGET' else 'dont update' end	
+							'FIBER TARGET 31/32/33' else 'dont update 31/32/33' end	
 					end																									--rows 31,32,33
 				else 
-					case when campuses_specif_recip_nonfiber_not_ia_lines_alloc = 0 then								--row 34
+					case when 	campuses_specif_recip_nonfiber_not_ia_lines_alloc = 0  or
+								fiber_wan_lines_w_dirty >= sum_alloc_wan_fiber_lines or 
+								fiber_wan_lines_w_dirty >= count_ben_wan_fiber_lines then								--row 34
 						case 	when 	exclude_from_analysis = false and 
 										priority_status__c not in ('Priority 1','Priority 3') and
 										(not(array_to_string(flag_array,',') ilike '%wan%')
-										or flag_array is null) then 'CLOSED LOST'
+										or flag_array is null) then 'CLOSED LOST 36/37'
 								when 	exclude_from_analysis = true and 
 										priority_status__c not in ('Priority 1','Priority 3') and
-										array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL'					--row 35
-						 		else 'dont update' end																	--rows 36,37
+										array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL 36/37'			--row 35
+						 		else 'dont update 36/37' end															--rows 36,37
 					else 
 						case when 	district_recip_clean_nonfiber_lines > 0 and 
 									(not('district_missing_ia' = any(flag_array)) or flag_array is null) and
 									(not('district_isp_missing_upstream' = any(flag_array)) or flag_array is null) then
-							'FIBER TARGET' else 'dont update' end	
+							'FIBER TARGET 38/39/40' else 'dont update 38/39/40' end	
 					end																									--rows 38,39,40
 				end
 			end
@@ -91,11 +97,11 @@ select
 		case 	when 	exclude_from_analysis = false and 
 						priority_status__c not in ('Priority 1','Priority 3') and
 						(not(array_to_string(flag_array,',') ilike '%wan%')
-						or flag_array is null) then 'CLOSED LOST'
+						or flag_array is null) then 'CLOSED LOST 42/43'
 				when 	exclude_from_analysis = true and 
 						priority_status__c not in ('Priority 1','Priority 3') and
-						array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL'									--row 41
-		 		else 'dont update' end																					--rows 42,43
+						array_to_string(flag_array,',') ilike '%wan%' then 'POTENTIAL 42/43'							--row 41
+		 		else 'dont update 42/43' end																			--rows 42,43
 	end as stage_indicator,
 	non_fiber_lines_w_dirty,
 	non_fiber_internet_upstream_lines_w_dirty,
@@ -233,8 +239,61 @@ left join (
 				ds.campus_id
 	) campus_agg
 	group by esh_id
-) clean_campus_recips
-on d.esh_id = clean_campus_recips.esh_id
+) campus_recips
+on d.esh_id = campus_recips.esh_id
+left join (
+		select  d.esh_id,
+				sum(case
+						when not(li.connect_category ilike '%Fiber%')
+							then alloc.original_num_lines_to_allocate
+					end) as sum_alloc_nonfiber_lines,
+				sum(case
+						when li.wan_conditions_met = true 
+							and li.connect_category ilike '%Fiber%'		
+							then alloc.original_num_lines_to_allocate
+					end) as sum_alloc_wan_fiber_lines,
+				sum(case
+						when (li.internet_conditions_met = true or li.upstream_conditions_met = true)
+							and li.connect_category ilike '%Fiber%'		
+							then alloc.original_num_lines_to_allocate
+					end) as sum_alloc_ia_fiber_lines,
+				count(distinct 	case
+									when not(li.connect_category ilike '%Fiber%')
+										then alloc.recipient_ben
+								end) as count_ben_nonfiber_lines,
+				count(distinct 	case
+									when li.wan_conditions_met = true 
+										and li.connect_category ilike '%Fiber%'		
+										then alloc.recipient_ben
+								end) as count_ben_wan_fiber_lines,
+				count(distinct 	case
+									when (li.internet_conditions_met = true or li.upstream_conditions_met = true)
+										and li.connect_category ilike '%Fiber%'		
+										then alloc.recipient_ben
+								end) as count_ben_ia_fiber_lines
+		from fy2016_districts_demog_ma d
+		left join public.fy2016_lines_to_district_by_line_item_ma ldli
+		on d.esh_id = ldli.district_esh_id
+		left join fy2016.line_items li
+		on ldli.line_item_id = li.id
+		left join (
+			select 	*
+			from fy2016.allocations
+			where broadband = true
+			and recipient_type in ('School', 'District')
+		) alloc
+		on ldli.line_item_id = alloc.line_item_id
+		where li.isp_conditions_met = false
+		and li.backbone_conditions_met = false
+		and li.consortium_shared = false
+		and (not('canceled' = any(li.open_flag_labels) or 
+		        'video_conferencing' = any(li.open_flag_labels) or
+		        'exclude' = any(li.open_flag_labels))
+				or li.open_flag_labels is null)
+		and district_esh_id is not null
+		group by d.esh_id		
+) district_alloc_recips
+on d.esh_id = district_alloc_recips.esh_id
 where district_type = 'Traditional'
 or (postal_cd = 'AZ' and district_type = 'Charter')
 
---
+++ Views/Materialized/2016 Material Girl Equivalents/fy2016_districts_metrics_mat.sql
@@ -0,0 +1,283 @@
+select	dd.*,
+				case 
+		          when dd.district_size in ('Tiny', 'Small') then 1
+		          when dd.district_size = 'Medium' then 1.5
+		          when dd.district_size = 'Large' then 1.75
+		          when dd.district_size = 'Mega' then 2.25
+		        end as ia_oversub_ratio,	
+				case
+					when da.campus_count is null
+						then num_schools
+					else da.campus_count
+				end as num_campuses,
+				da.flag_array,
+				da.tag_array,
+				broadband_internet_upstream_lines,							
+				case											
+					when	com_info_bandwidth	>	0								
+						then	com_info_bandwidth										
+					when	upstream_bandwidth	=	0								
+						then	isp_bandwidth										
+					when	isp_bandwidth	=	0								
+						then	upstream_bandwidth										
+					when	upstream_bandwidth	>	isp_bandwidth								
+						then	isp_bandwidth										
+					else	upstream_bandwidth										
+				end	+	internet_bandwidth	as	ia_bandwidth,							
+				case
+					when num_students != 0
+						then (case											
+										when	com_info_bandwidth	>	0								
+											then	com_info_bandwidth										
+										when	upstream_bandwidth	=	0								
+											then	isp_bandwidth										
+										when	isp_bandwidth	=	0								
+											then	upstream_bandwidth										
+										when	upstream_bandwidth	>	isp_bandwidth								
+											then	isp_bandwidth										
+										else	upstream_bandwidth										
+									end	+	internet_bandwidth)/num_students*1000
+				end as ia_bandwidth_per_student_kbps,								
+				case											
+					when	com_info_bandwidth_cost	>	0								
+						then	com_info_bandwidth_cost										
+					when	upstream_bandwidth_cost	=	0								
+						then	isp_bandwidth_cost										
+					when	isp_bandwidth_cost	=	0								
+						then	upstream_bandwidth_cost										
+					when	upstream_bandwidth_cost	>	isp_bandwidth_cost								
+						then	isp_bandwidth_cost										
+					else	upstream_bandwidth_cost										
+				end	+	internet_bandwidth_cost	as	ia_bandwidth_for_cost_per_mbps,	
+				ia_monthly_cost_direct_to_district	+	
+				    			(ia_monthly_cost_per_student_backbone_pieces*num_students) as ia_monthly_cost,
+				ia_monthly_cost_direct_to_district,
+				ia_monthly_cost_per_student_backbone_pieces*num_students as ia_monthly_cost_shared,    													
+				case											
+				  when (case											
+			          	when	com_info_bandwidth_cost	>	0								
+			          	then	com_info_bandwidth_cost										
+			          	when	upstream_bandwidth_cost	=	0								
+			          	then	isp_bandwidth_cost										
+			          	when	isp_bandwidth_cost	=	0								
+			          	then	upstream_bandwidth_cost										
+			          	when	upstream_bandwidth_cost	>	isp_bandwidth_cost								
+			          	then	isp_bandwidth_cost										
+			          	else	upstream_bandwidth_cost										
+			          end	+	internet_bandwidth_cost) > 0									
+				    then  (ia_monthly_cost_direct_to_district	+	
+				    			(ia_monthly_cost_per_student_backbone_pieces*num_students))/
+				    				(case									
+                    	when	com_info_bandwidth_cost	>	0								
+                    	then	com_info_bandwidth_cost										
+                    	when	upstream_bandwidth_cost	=	0								
+                    	then	isp_bandwidth_cost										
+                    	when	isp_bandwidth_cost	=	0								
+                    	then	upstream_bandwidth_cost										
+                    	when	upstream_bandwidth_cost	>	isp_bandwidth_cost								
+                    	then	isp_bandwidth_cost										
+                    	else	upstream_bandwidth_cost										
+                    end	+	internet_bandwidth_cost)					
+			  end as	ia_monthly_cost_per_mbps,
+				case											
+				  when wan_lines_cost > 0									
+				    then  wan_monthly_cost/wan_lines_cost					
+			  end as	wan_monthly_cost_per_line,
+			COALESCE (
+			    case when (all_ia_connectcat ILIKE '%Fiber%') then 'Fiber' else NULL end,
+			    case when (all_ia_connectcat ILIKE '%Fixed Wireless%') then 'Fixed Wireless' else NULL end,
+			    case when (all_ia_connectcat ILIKE '%Cable%') then 'Cable' else NULL end,
+			    case when ( all_ia_connectcat ILIKE '%DSL%' or 
+			    			all_ia_connectcat ILIKE '%Copper%' or 
+			    			all_ia_connectcat ILIKE '%T-1%') then 'Copper' else NULL end,
+				case when (all_ia_connectcat ILIKE '%Satellite/LTE%') then 'Satellite/LTE' else NULL end,
+			    case when (all_ia_connectcat ILIKE '%Uncategorized%') then 'Uncategorized' else 'None - Error' end
+			) as hierarchy_connect_category,
+			all_ia_connectcat,
+		    case
+		      when campus_count < fiber_lines + fixed_wireless_lines + 	case 
+		      																when num_students < 100 
+		      																	then cable_lines 
+		      																else 0 
+		      															end
+		        then campus_count 
+		        else fiber_lines + fixed_wireless_lines + 	case 
+																when num_students < 100 
+																	then cable_lines 
+																else 0 
+															end
+		    end as known_scalable_campuses,
+		    case 
+		      when num_schools > 5 and wan_lines = 0 
+		        then 
+		          case 
+		            when campus_count > fiber_lines + fixed_wireless_lines + 	case 
+																					when num_students < 100 
+																						then cable_lines 
+																					else 0 
+																				end
+		              then campus_count - fiber_lines + fixed_wireless_lines + 	case 
+																					when num_students < 100 
+																						then cable_lines 
+																					else 0 
+																				end
+		              else 0
+		          end
+		        else 0
+		    end as assumed_scalable_campuses,
+		    case
+		      when num_students < 100 and copper_dsl_lines > 0 and not(num_schools > 5 and wan_lines = 0 )
+		        then 
+		          case
+		            when campus_count < (fiber_lines + fixed_wireless_lines + cable_lines)
+		              then 0
+		            when campus_count - (fiber_lines + fixed_wireless_lines + cable_lines) < copper_dsl_lines
+		              then campus_count - (fiber_lines + fixed_wireless_lines + cable_lines)
+		              else copper_dsl_lines
+		          end
+		      when num_students >= 100 and (copper_dsl_lines + cable_lines)> 0  and not(num_schools > 5 and wan_lines = 0 ) 
+		        then
+		          case
+		            when campus_count < (fiber_lines + fixed_wireless_lines)
+		              then 0
+		            when campus_count - (fiber_lines + fixed_wireless_lines ) < copper_dsl_lines + cable_lines
+		              then campus_count - (fiber_lines + fixed_wireless_lines)
+		              else copper_dsl_lines + cable_lines
+		          end
+		        else 0
+		    end as known_unscalable_campuses,
+		    case 
+		      when num_schools > 5 and wan_lines = 0 
+		        then 0 
+		        else 
+		          case
+		            when campus_count < (fiber_lines + fixed_wireless_lines + copper_dsl_lines + cable_lines)
+		              then 0
+		              else campus_count - (fiber_lines + fixed_wireless_lines + copper_dsl_lines + cable_lines)
+		          end
+		    end as assumed_unscalable_campuses,
+		      case
+		        when campus_count < fiber_lines 
+		          then campus_count 
+		          else fiber_lines 
+		      end as nga_v2_known_scalable_campuses,
+		      case 
+		        when num_schools > 5 and wan_lines = 0 
+		          then 
+		            case 
+		              when campus_count > fiber_lines 
+		                then campus_count - fiber_lines 
+		                else 0
+		            end
+		          else 0
+		      end as nga_v2_assumed_scalable_campuses,
+		      case
+		        when copper_dsl_lines > 0 and not(num_schools > 5 and wan_lines = 0 )
+		          then 
+		            case
+		              when campus_count < (fiber_lines )
+		                then 0
+		              when campus_count - (fiber_lines ) < copper_dsl_lines
+		                then campus_count - (fiber_lines)
+		                else copper_dsl_lines
+		            end
+		          else 0
+		      end as nga_v2_known_unscalable_campuses,
+		      case 
+		        when num_schools > 5 and wan_lines = 0 
+		          then 0 
+		          else 
+		            case
+		              when campus_count < (fiber_lines + copper_dsl_lines)
+		                then 0
+		                else campus_count - (fiber_lines + copper_dsl_lines)
+		            end
+		      end as nga_v2_assumed_unscalable_campuses,
+      case
+        when campus_count < fiber_lines
+          then campus_count 
+          else fiber_lines
+      end as known_fiber_campuses,
+      case 
+        when num_schools > 5 and wan_lines = 0 
+          then 
+            case 
+              when campus_count > fiber_lines 
+                then campus_count - fiber_lines
+                else 0
+            end
+          else 0
+      end as assumed_fiber_campuses,
+      case
+        when non_fiber_lines > 0 and not(num_schools > 5 and wan_lines = 0 )
+          then 
+            case
+              when campus_count < fiber_lines
+                then 0
+              when campus_count - fiber_lines < non_fiber_lines
+                then campus_count - fiber_lines 
+                else non_fiber_lines
+            end
+          else 0
+      end as known_nonfiber_campuses,
+      case 
+        when num_schools > 5 and wan_lines = 0 
+          then 0 
+          else 
+            case
+              when campus_count < fiber_lines + non_fiber_lines
+                then 0
+                else campus_count - (fiber_lines + non_fiber_lines)
+            end
+      end as assumed_nonfiber_campuses,
+			fiber_internet_upstream_lines,
+			fixed_wireless_internet_upstream_lines,
+			cable_internet_upstream_lines,
+			copper_internet_upstream_lines,
+			satellite_lte_internet_upstream_lines,
+			uncategorized_internet_upstream_lines,
+			wan_lines,
+			wan_bandwidth_low,
+			wan_bandwidth_high,
+			gt_1g_wan_lines,
+			lt_1g_fiber_wan_lines,
+			lt_1g_nonfiber_wan_lines,
+			ia_applicants,
+			dedicated_isp_sp,
+			dedicated_isp_services,
+			dedicated_isp_contract_expiration,
+			bundled_internet_sp,
+			bundled_internet_services,
+			bundled_internet_contract_expiration,
+			upstream_sp,
+			upstream_services,
+			upstream_contract_expiration,
+			wan_applicants,
+			wan_sp,
+			wan_services,
+			wan_contract_expiration,
+			frl_percent,
+		    c1_discount_rate as discount_rate_c1,
+		    c2_discount_rate as discount_rate_c2,
+		    flag_count,
+		    wan_monthly_cost,
+		    machine_cleaned_lines,
+		    non_fiber_lines,
+		    non_fiber_lines_w_dirty,
+		    non_fiber_internet_upstream_lines_w_dirty,
+		    fiber_internet_upstream_lines_w_dirty,
+		    fiber_wan_lines_w_dirty,
+		    lines_w_dirty									
+												
+from	fy2016_districts_demog_ma		 dd									
+left	join	fy2016_districts_aggregation_mat	da									
+on	dd.esh_id	=	da.district_esh_id									
+
+/*
+Author: Justine Schott
+Created On Date: 6/20/2016
+Last Modified Date: 8/26/2016
+Name of QAing Analyst(s): 
+Purpose: Districts in 2016 universe, including metric calculations and cleanliness
+Methodology: Utilizing other aggregation tables
+*/
