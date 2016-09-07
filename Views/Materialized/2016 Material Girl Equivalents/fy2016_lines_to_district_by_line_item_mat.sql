3dcaa6baab3922f4730ba4907f89edb42d095046
justineschott <justine@educationsuperhighway.org>
2016-09-06 21:45:57 -0800

    Update
    for new lookup table

--- Views/Materialized/2016 Material Girl Equivalents/fy2016_lines_to_district_by_line_item_mat.sql
+++ Views/Materialized/2016 Material Girl Equivalents/fy2016_lines_to_district_by_line_item_mat.sql
@@ -5,7 +5,7 @@ select dl.district_esh_id,
 from fy2016.entity_circuits ec
 join fy2016.circuits c
 on ec.circuit_id = c.id
-join fy2016_district_lookup_ma dl
+join fy2016_district_lookup_mat dl
 on ec.entity_id::varchar = dl.esh_id
 
 group by  district_esh_id,
@@ -14,7 +14,7 @@ group by  district_esh_id,
 /*
 Author:                       Justine Schott
 Created On Date:              06/16/2016
-Last Modified Date: 		  08/26/2016
+Last Modified Date: 		  09/06/2016
 Name of QAing Analyst(s):  
 Purpose:                      To aggregate services received by all instructional facilities to the appropriate district. (2016)
 */
