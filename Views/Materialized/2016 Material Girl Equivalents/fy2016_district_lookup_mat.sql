9746f7f9a72b65abb6435ed60fcc4ef776dd511e
justineschott <justine@educationsuperhighway.org>
2016-09-14 10:57:14 -0800

    per devon...

--- Views/Materialized/2016 Material Girl Equivalents/fy2016_district_lookup_mat.sql
+++ Views/Materialized/2016 Material Girl Equivalents/fy2016_district_lookup_mat.sql
@@ -2,9 +2,9 @@ select esh_id, esh_id as district_esh_id
 from fy2016_districts_demog_m
   union
 select esh_id::varchar, district_esh_id::varchar
-from fy2016.schools
-where max_grade_level != 'PK'
-and charter = false --note: TEMPORARILY (materialized only) using the set of schools ENG is using in order to be aligned on services received
+from fy2016.schools sd
+join fy2016_districts_demog_m dd
+on sd.district_esh_id::varchar = dd.esh_id--note: TEMPORARILY (materialized only) using the set of schools ENG is using in order to be aligned on services received
 
 /*
 Author:                       Justine Schott
