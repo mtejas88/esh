0c26f53a54ac93ac396ed8f462c26bcf246991d6
justineschott <justine@educationsuperhighway.org>
2016-09-14 10:58:59 -0800

    final

--- Views/Materialized/2016 Material Girl Equivalents/fy2016_district_lookup_mat.sql
+++ Views/Materialized/2016 Material Girl Equivalents/fy2016_district_lookup_mat.sql
@@ -1,7 +1,7 @@
 select esh_id, esh_id as district_esh_id
 from fy2016_districts_demog_m
   union
-select esh_id::varchar, district_esh_id::varchar
+select sd.esh_id::varchar, sd.district_esh_id::varchar
 from fy2016.schools sd
 join fy2016_districts_demog_m dd
 on sd.district_esh_id::varchar = dd.esh_id--note: TEMPORARILY (materialized only) using the set of schools ENG is using in order to be aligned on services received
