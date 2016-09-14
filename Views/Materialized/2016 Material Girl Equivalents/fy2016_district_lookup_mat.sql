d18400a05f15884274e01e00b4fc1031c385ea22
justineschott <justine@educationsuperhighway.org>
2016-09-14 10:54:08 -0800

    crusher format

--- Views/Materialized/2016 Material Girl Equivalents/fy2016_district_lookup_mat.sql
+++ Views/Materialized/2016 Material Girl Equivalents/fy2016_district_lookup_mat.sql
@@ -1,7 +1,7 @@
 select esh_id, esh_id as district_esh_id
 from fy2016_districts_demog_m
   union
-select school_esh_id as esh_id, district_esh_id
+select esh_id::varchar, district_esh_id::varchar
 from fy2016.schools
 where max_grade_level != 'PK'
 and charter = false --note: TEMPORARILY (materialized only) using the set of schools ENG is using in order to be aligned on services received
