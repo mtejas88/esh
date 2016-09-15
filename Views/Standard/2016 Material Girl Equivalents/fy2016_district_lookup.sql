4c491ec9200a1fabfe4bff04b503528addb72eea
justineschott <justine@educationsuperhighway.org>
2016-09-15 10:39:05 -0800

    Update district lookup non-materialized

--- Views/Standard/2016 Material Girl Equivalents/fy2016_district_lookup.sql
+++ Views/Standard/2016 Material Girl Equivalents/fy2016_district_lookup.sql
@@ -1,14 +1,15 @@
 select esh_id, esh_id as district_esh_id
 from fy2016_districts_demog
   union
-select school_esh_id as esh_id, district_esh_id
-from fy2016_schools_demog
-where self_procuring_charter = false
+select sd.esh_id::varchar, sd.district_esh_id::varchar
+from fy2016.schools sd
+join fy2016_districts_demog dd
+on sd.district_esh_id::varchar = dd.esh_id--note: TEMPORARILY (materialized only) using the set of schools ENG is using in order to be aligned on services received
 
 /*
 Author:                       Justine Schott
 Created On Date:              06/16/2016
-Last Modified Date: 		  09/06/2016 
+Last Modified Date: 		  09/15/2016 
 Name of QAing Analyst(s):  
 Purpose:                      To append a list of all instructonal facilities and their associated district_esh_id 
                               in order to aggregate services received by all these to the appropriate district (2016)
