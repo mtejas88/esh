d03452fb6b1789360dc0cf275e9e9ee31ec40330
justineschott <justine@educationsuperhighway.org>
2016-11-10 16:28:58 -0800

    typo update
    deluxe>predeluxe

--- Views/Materialized/2016 Material Girl Equivalents/fy2016_districts_deluxe_matr.sql
+++ Views/Materialized/2016 Material Girl Equivalents/fy2016_districts_deluxe_matr.sql
@@ -200,7 +200,7 @@ select distinct
 	fbts.fiber_target_status,
   	fbts.bw_target_status
 
-from public.fy2016_districts_deluxe_matr dpd
+from public.fy2016_districts_predeluxe_matr dpd
 left join public.fy2016_fiber_bw_target_status fbts
 on dpd.esh_id = fbts.esh_id
 
