42e7a638e97615c4344e719638b59424acc26da0
justineschott <justine@educationsuperhighway.org>
2016-11-28 12:43:19 -0800

    Updates to metrics and deluxe
    Removing unnecessary columns and adding district metrics where more
    applicable (ie for cleanliness)

--- Views/Materialized/fy2016_schools_deluxe_matr.sql
+++ Views/Materialized/fy2016_schools_deluxe_matr.sql
@@ -1,9 +1,8 @@
-select 
-	distinct sm.school_esh_id,
-   sm.district_esh_id,
+select
+	distinct
    sm.campus_id,
-   sm.school_nces_code,
-   sm.district_include_in_universe_of_districts,
+   sm.school_esh_ids,
+   sm.district_esh_id,
    sm.name,
    sm.school_type,
    sm.address,
@@ -13,18 +12,11 @@ select
    sm.locale,
    sm.num_students,
    sm.frl_percent,
-   sm.discount_rate_c1,
-   sm.discount_rate_c2,
-	   case when (flag_array is null or 
-	   			 (flag_count = 1 
-	   			 	and array_to_string(flag_array,',') ilike '%missing_wan%')) 
-	   			 and ia_bandwidth_per_student_kbps > 0
+   case
+   		when dd.exclude_from_ia_analysis = false and ia_bandwidth > 0
 			then false
 		else true
    end as exclude_from_ia_analysis,
-	flag_array,
-	tag_array,
-	flag_count as num_open_district_flags,
 	ia_bandwidth_per_student_kbps,
 	case
 		when ia_bandwidth_per_student_kbps >= 100
@@ -46,13 +38,6 @@ select
 			then true
 		else false
 	end as at_least_one_line_not_meeting_broadband_goal,
-	case
-		when 'bw_upgrade' = any(tag_array)
-			then true
-		when 'bw_not_upgrade' = any(tag_array)
-			then false
-		else null
-	end as bw_upgrade_indicator,
 	ia_monthly_cost_per_mbps,
 	ia_bandwidth as ia_bw_mbps_total,
 	ia_monthly_cost as ia_monthly_cost_total,
@@ -68,39 +53,13 @@ select
 	current_assumed_scalable_campuses,
 	current_known_unscalable_campuses,
 	current_assumed_unscalable_campuses,
-	sots_known_scalable_campuses,
-	sots_assumed_scalable_campuses,
-	sots_known_unscalable_campuses,
-	sots_assumed_unscalable_campuses,
 	wan_lines,
 	non_fiber_lines,
-    non_fiber_lines_w_dirty,
-    non_fiber_internet_upstream_lines_w_dirty,
-    fiber_internet_upstream_lines_w_dirty,
-    fiber_wan_lines_w_dirty,
-	lines_w_dirty,
-	fiber_wan_lines,
-	
-	ia_monthly_cost_no_backbone,
-	CASE 	WHEN wifi.count_wifi_needed > 0 THEN true
-   			WHEN wifi.count_wifi_needed = 0 THEN false
-        	ELSE null
-		   	END as needs_wifi,
-	c2.c2_prediscount_budget_15,
-	c2.c2_prediscount_remaining_15,
-	c2.c2_prediscount_remaining_16,
-	c2.c2_postdiscount_remaining_15,
-	c2.c2_postdiscount_remaining_16,
-	c2.received_c2_15,
-	c2.received_c2_16,
-	c2.budget_used_c2_15,
-	c2.budget_used_c2_16
+	ia_monthly_cost_no_backbone
 
 from public.fy2016_schools_metrics_matr sm
-	left join public.fy2016_wifi_connectivity_informations_matr wifi
-	on sm.district_esh_id = wifi.parent_entity_id::varchar
-	left join public.fy2016_districts_c2_funding_matr c2
-	on sm.district_esh_id = c2.esh_id::varchar
+left join public.fy2016_districts_deluxe dd
+on sm.district_esh_id = dd.esh_id
 
 order by sm.postal_cd,
 		 sm.campus_id
@@ -108,6 +67,6 @@ order by sm.postal_cd,
 /*
 Author: Jess Seok
 Created On Date: 11/18/2016
-Last Modified Date: 
+Last Modified Date:
 Name of QAing Analyst(s):Justine Schott
 */
--- Views/Materialized/fy2016_schools_metrics_matr.sql
+++ Views/Materialized/fy2016_schools_metrics_matr.sql
@@ -1,15 +1,7 @@
-select	sd.*,  --all school demographic information!--  -- ia bandwidth per student 
--- ia monthly cost per Mbps --  
--- fiber?
-				case
-					when campus_count is null
-						then 1  -- since each dataset is a school :)
-					else campus_count
-				end as num_campuses,
-				sa.flag_array,
-				sa.tag_array,
-				sa.broadband_internet_upstream_lines,
-				sa.not_broadband_internet_upstream_lines,
+select			sd.*,
+				1 as num_campuses,
+				broadband_internet_upstream_lines,
+				not_broadband_internet_upstream_lines,
 				case
 					when com_info_bandwidth	>	0
 						then com_info_bandwidth
@@ -82,116 +74,38 @@ select	sd.*,  --all school demographic information!--  -- ia bandwidth per stude
                     end	+	internet_bandwidth_cost)
 			  end as	ia_monthly_cost_per_mbps,
 		    case
-		      when campus_count < fiber_lines + fixed_wireless_lines + satellite_lte_lines + 	case
-								      																when num_students < 100
-								      																	then cable_lines
-								      																else 0
-								      															end
-		        then campus_count
-		        else fiber_lines + fixed_wireless_lines + satellite_lte_lines +	case
-																					when num_students < 100
-																						then cable_lines
-																					else 0
-																				end
-		    end as sots_known_scalable_campuses,
-		    case
-		      when num_schools > 5 and wan_lines = 0
-		        then
-		          case
-		            when campus_count > fiber_lines + fixed_wireless_lines + satellite_lte_lines + case
-																										when num_students < 100
-																											then cable_lines
-																										else 0
-																									end
-		              then campus_count - (fiber_lines + fixed_wireless_lines + satellite_lte_lines + 	case
-																										when num_students < 100
-																											then cable_lines
-																										else 0
-																									end)
-		              else 0
-		          end
-		        else 0
-		    end as sots_assumed_scalable_campuses,
-		    case
-		      when num_students < 100 and copper_dsl_lines > 0 and not(num_schools > 5 and wan_lines = 0 )
-		        then
-		          case
-		            when campus_count < (fiber_lines + fixed_wireless_lines + satellite_lte_lines + cable_lines)
-		              then 0
-		            when campus_count - (fiber_lines + fixed_wireless_lines + satellite_lte_lines + cable_lines) < copper_dsl_lines
-		              then campus_count - (fiber_lines + fixed_wireless_lines + satellite_lte_lines + cable_lines)
-		              else copper_dsl_lines
-		          end
-		      when num_students >= 100 and (copper_dsl_lines + cable_lines)> 0  and not(num_schools > 5 and wan_lines = 0 )
-		        then
-		          case
-		            when campus_count < (fiber_lines + fixed_wireless_lines + satellite_lte_lines)
-		              then 0
-		            when campus_count - (fiber_lines + fixed_wireless_lines + satellite_lte_lines) < copper_dsl_lines + cable_lines
-		              then campus_count - (fiber_lines + fixed_wireless_lines + satellite_lte_lines)
-		              else copper_dsl_lines + cable_lines
-		          end
-		        else 0
-		    end as sots_known_unscalable_campuses,
-		    case
-		      when num_schools > 5 and wan_lines = 0
-		        then 0
-		        else
-		          case
-		            when campus_count < (fiber_lines + fixed_wireless_lines + satellite_lte_lines + copper_dsl_lines + cable_lines)
-		              then 0
-		              else campus_count - (fiber_lines + fixed_wireless_lines + satellite_lte_lines + copper_dsl_lines + cable_lines)
-		          end
-		    end as sots_assumed_unscalable_campuses,
-		    case
-		      when campus_count < fiber_lines
-		        then campus_count
-		        else fiber_lines
-		    end as current_known_scalable_campuses,
-		    case
 		      when copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines > 0
 		        then
 		          case
-		            when campus_count < (fiber_lines )
+		            when 1 < (fiber_lines )
 		              then 0
-		            when campus_count - (fiber_lines ) < copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines
-		              then campus_count - (fiber_lines)
+		            when 1 - (fiber_lines ) < copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines
+		              then 1 - (fiber_lines)
 		              else copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines
 		          end
 		        else 0
 		    end as current_known_unscalable_campuses,
             case
-              when campus_count < fiber_lines + copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines
+              when 1 < fiber_lines + copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines
                 then 0
-              else .92* (campus_count - (fiber_lines + copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines))::numeric
+              else .92* (1 - (fiber_lines + copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines))::numeric
             end as current_assumed_scalable_campuses,
             case
-              when campus_count < fiber_lines + copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines
+              when 1 < fiber_lines + copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines
                 then 0
-              else .08* (campus_count - (fiber_lines + copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines))::numeric
+              else .08* (1 - (fiber_lines + copper_dsl_lines + satellite_lte_lines + fixed_wireless_lines + cable_lines))::numeric
             end as current_assumed_unscalable_campuses,
 			wan_lines,
-			frl_percent,
-		    c1_discount_rate as discount_rate_c1,
-		    c2_discount_rate as discount_rate_c2,
-		    flag_count,
-		    non_fiber_lines,
-		    non_fiber_lines_w_dirty,
-		    non_fiber_internet_upstream_lines_w_dirty,
-		    fiber_internet_upstream_lines_w_dirty,
-		    fiber_wan_lines_w_dirty,
-		    lines_w_dirty,
-		    line_items_w_dirty,
-		    fiber_wan_lines
+			frl_percent
 
-from fy2016_schools_demog_matr as sd
+from public.fy2016_schools_demog_matr as sd
 left join public.fy2016_schools_aggregation_matr as sa
 on	sd.school_esh_id = sa.school_esh_id
-where sd.postal_cd in ('DE', 'HI') 
+where sd.postal_cd in ('DE', 'HI', 'RI')
 
 /*
 Author: Jess Seok
 Created On Date: 11/18/2016
-Last Modified Date: 
+Last Modified Date:
 Name of QAing Analyst(s):Justine Schott
 */
