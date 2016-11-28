bae9e4a2ae1958f3d7f8fd3e3edea0974c977b7c
Justine <justine@educationsuperhighway.org>
2016-11-28 08:26:59 -0800

    Updates
    Note -- NOT 100% CORRECT -- this is WIP. more details coming

--- Views/Materialized/fy2016_lines_to_school_by_line_item_matr.sql
+++ Views/Materialized/fy2016_lines_to_school_by_line_item_matr.sql
@@ -1,12 +1,16 @@
-select ec.entity_id as school_esh_id,
+select sd.campus_id,
+	   sd.district_esh_id,
        c.line_item_id,
        count(distinct circuit_id) as allocation_lines
         
 from fy2016.entity_circuits ec
 join fy2016.circuits c
 on ec.circuit_id = c.id
+join public.fy2016_schools_demog_matr sd
+on ec.entity_id::varchar = sd.school_esh_id
 
-group by  school_esh_id,
+group by  campus_id,
+	   	  district_esh_id,
           line_item_id
 
 /*
--- Views/Materialized/fy2016_schools_aggregation_matr.sql
+++ Views/Materialized/fy2016_schools_aggregation_matr.sql
@@ -1,13 +1,9 @@
-select  		sd.school_esh_id::varchar,
-				    sd.district_esh_id,
-				    campus_count,
-				    1 as num_schools,
-        	frl_percent,
-			    flag_array,
-			    flag_count,
-			    tag_array,
-			    c1_discount_rate,
-			    c2_discount_rate,
+select  		sd.campus_id,
+				sd.postal_cd,
+				sd.school_esh_ids, 
+				sd.district_esh_id,
+				sd.num_schools,
+        		sd.frl_percent,
 --ia bw/student pieces
 				sum(case
 							when	'committed_information_rate'	=	any(open_tag_labels)
@@ -197,180 +193,76 @@ select  		sd.school_esh_id::varchar,
 									and consortium_shared = false
 										then	allocation_lines
 									else	0
-								end) as non_fiber_lines,
---clean and dirty for stage_indicator
-						sum(case
-									when (not(connect_category ilike '%fiber%')
-										or connect_type in ('DS-1', 'Digital Subscriber Line (DSL)'))
-									and isp_conditions_met = false
-									and backbone_conditions_met = false
-									and consortium_shared = false
-										then	allocation_lines
-									else	0
-								end) as non_fiber_lines_w_dirty,
-						sum(case
-									when (not(connect_category ilike '%fiber%')
-										or connect_type in ('DS-1', 'Digital Subscriber Line (DSL)'))
-									and (internet_conditions_met = true or upstream_conditions_met = true)
-									and consortium_shared = false
-										then	allocation_lines
-									else	0
-								end) as non_fiber_internet_upstream_lines_w_dirty,
-						sum(case
-									when connect_category ILIKE '%Fiber%'
-									and connect_type not in ('DS-1', 'Digital Subscriber Line (DSL)')
-									and (internet_conditions_met = true or upstream_conditions_met = true)
-									and consortium_shared = false
-										then	allocation_lines
-									else	0
-								end) as fiber_internet_upstream_lines_w_dirty,
-						sum(case
-									when connect_category ILIKE '%Fiber%'
-									and connect_type not in ('DS-1', 'Digital Subscriber Line (DSL)')
-									and wan_conditions_met = true
-									and consortium_shared = false
-										then	allocation_lines
-									else	0
-								end) as fiber_wan_lines_w_dirty,
-						sum(case
-									when isp_conditions_met = false
-									and backbone_conditions_met = false
-									and consortium_shared = false
-										then	allocation_lines
-									else	0
-								end) as lines_w_dirty,
-						sum(allocation_lines) as line_items_w_dirty,
-						sum(case
-									when num_open_flags	=	0
-									and connect_category ILIKE '%Fiber%'
-									and connect_type not in ('DS-1', 'Digital Subscriber Line (DSL)')
-									and wan_conditions_met = true
-									and consortium_shared = false
-										then	allocation_lines
-									else	0
-								end) as fiber_wan_lines,
---cleanliness
-						sum(case
-									when ('exclude_for_cost_only_free' = any(open_tag_labels) or 'exclude_for_cost_only_restricted'= any(open_tag_labels))
-									and (internet_conditions_met or upstream_conditions_met or isp_conditions_met or backbone_conditions_met)
-										then allocation_lines
-									else	0
-								end) as ia_no_cost_lines,
-						sum(case
-									when ('exclude_for_cost_only_free' = any(open_tag_labels) or 'exclude_for_cost_only_restricted'= any(open_tag_labels))
-									and wan_conditions_met
-										then allocation_lines
-									else	0
-								end) as wan_no_cost_lines--,
-	--					include_in_universe_of_districts
+								end) as non_fiber_lines
 
-from endpoint.fy2016_schools_demog sd
-	left join endpoint.fy2016_lines_to_school_by_line_item as lsli
-	on 	sd.school_esh_id::varchar = lsli.school_esh_id::varchar
-
-	left join	(
-
-		select *
-		from fy2016.line_items 
-		where broadband = true
-		and (not('canceled' = any(open_flag_labels) or
-		        'video_conferencing' = any(open_flag_labels) or
-		        'exclude' = any(open_flag_labels)) or 
-			open_flag_labels is null)
-
-	)	li
-	on	lsli.line_item_id	=	li.id
-
-	left join (
-
-		select	lsli.line_item_id,
-				sum(s.num_students::numeric) as	num_students_served
-		from endpoint.fy2016_lines_to_school_by_line_item as	lsli
-			join endpoint.fy2016_schools_demog s
-			on lsli.school_esh_id::varchar	=	s.school_esh_id::varchar
-
-			join fy2016.line_items	li
-			on lsli.line_item_id	=	li.id
-
-		where (li.consortium_shared=true or backbone_conditions_met = true)
-		and broadband = true
-		group	by	lsli.line_item_id
-	
-	) school_info_by_li
-	on	school_info_by_li.line_item_id	=	lsli.line_item_id
-
-	left join (
-		select	school_esh_id,
-				count(distinct 	case
-									when campus_id is null or campus_id = 'Unknown'
-										then address
-									else campus_id
-								end) as campus_count,
-				case
-					when sum(frl_percentage_denomenator) > 0
+from (
+	select 	postal_cd,
+			case
+				when campus_id is null or campus_id = 'Unknown'
+					then address
+				else campus_id
+			end as campus_id,
+			district_esh_id,
+			array_agg(school_esh_id) as school_esh_ids,
+			count(*) as num_schools,
+			case
+				when sum(frl_percentage_denomenator) > 0
 					then sum(frl_percentage_numerator)/sum(	 frl_percentage_denomenator)
-				end as frl_percent
+			end as frl_percent
 
-		from endpoint.fy2016_schools_demog
-		group	by	school_esh_id
-	
-	) school_info
-	on	school_info.school_esh_id	=	sd.school_esh_id::varchar
-
-	left join (
-		select	flaggable_id,
-				array_agg(distinct label) as flag_array,
+    from public.fy2016_schools_demog_matr 
+    group by 	postal_cd,
 				case
-					when count(distinct label) is null
-						then 0
-					else count(distinct label)
-				end as flag_count
+					when campus_id is null or campus_id = 'Unknown'
+						then address
+					else campus_id
+				end,
+				district_esh_id
+ ) sd
+left join public.fy2016_lines_to_school_by_line_item_matr as lsli
+on 	sd.campus_id = lsli.campus_id
 
-		from fy2016.flags
-		where status = 'open'
-		group	by	flaggable_id
-	
-	) flag_info
-	on	flag_info.flaggable_id::varchar	= sd.district_esh_id
+left join	(
+	select *
+	from fy2016.line_items 
+	where broadband = true
+	and (not('canceled' = any(open_flag_labels) or
+	        'video_conferencing' = any(open_flag_labels) or
+	        'exclude' = any(open_flag_labels)) or 
+		open_flag_labels is null)
 
-	left join (
+)	li
+on	lsli.line_item_id	=	li.id
 
-		select	taggable_id,
-				array_agg(distinct label) as tag_array
-		from fy2016.tags
-		where deleted_at is null
-		group	by	taggable_id
+left join (
 
-	) tag_info	
-	on	tag_info.taggable_id::varchar = sd.district_esh_id
+	select	lsli.line_item_id,
+			sum(s.num_students::numeric) as	num_students_served
+	from public.fy2016_lines_to_school_by_line_item_matr as	lsli
+	join public.fy2016_schools_demog_matr s
+	on lsli.campus_id	=	s.campus_id
 
-	left join (
-		select	entity_id,
-				min(parent_category_one_discount_rate) as c1_discount_rate,
-				min(parent_category_two_discount_rate) as c2_discount_rate
-		from fy2016.discount_calculations dc
-			join ( select distinct entity_id, ben
-            	   from public.entity_bens) eim
-			on dc.parent_entity_ben = eim.ben
-		group by entity_id
-	) dr_info
-	on	dr_info.entity_id::varchar = sd.district_esh_id
+	join fy2016.line_items	li
+	on lsli.line_item_id	=	li.id
 
-where sd.postal_cd in ('DE', 'HI')
-group by	sd.school_esh_id::varchar,
-          sd.district_esh_id::varchar,
-			    campus_count,
-			    num_schools,
-        		frl_percent,
-			    flag_array,
-			    flag_count,
-			    tag_array,
-			    c1_discount_rate,
-			    c2_discount_rate
+	where (li.consortium_shared=true or backbone_conditions_met = true)
+	and broadband = true
+	group	by	lsli.line_item_id
+
+) school_info_by_li
+on	school_info_by_li.line_item_id	=	lsli.line_item_id
+
+where sd.postal_cd in ('DE', 'HI', 'RI')
+group by	sd.campus_id,
+			sd.postal_cd,
+			sd.school_esh_ids, 
+			sd.district_esh_id,
+			sd.num_schools,
+    		sd.frl_percent
 
 /*
 Author: Jess Seok
-Created On Date: 11/18/2016
+Created On Date: 11/21/2016
 Last Modified Date: 
 Name of QAing Analyst(s):Justine Schott
 */
