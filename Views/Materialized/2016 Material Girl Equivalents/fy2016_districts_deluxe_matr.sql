a4c2f81774dd3b97e36be01d05ab872cba69e2b5
justineschott <justine@educationsuperhighway.org>
2016-12-30 11:05:40 -0800

    Update for syntax

--- Views/Materialized/2016 Material Girl Equivalents/fy2016_districts_deluxe_matr.sql
+++ Views/Materialized/2016 Material Girl Equivalents/fy2016_districts_deluxe_matr.sql
@@ -94,7 +94,11 @@ select distinct
 	wan_monthly_cost_per_line,
 	wan_monthly_cost_total,
 	meeting_3_per_mbps_affordability_target,
-	affordability_calculator(ia_monthly_cost_total, ia_bw_mbps_total) as meeting_knapsack_affordability_target,
+	case
+		when ia_bw_mbps_total > 0
+			then affordability_calculator(ia_monthly_cost_total::integer, ia_bw_mbps_total::integer)
+		else false
+	end as meeting_knapsack_affordability_target,
 	hierarchy_ia_connect_category,
 	all_ia_connectcat,
 	case
@@ -258,7 +262,7 @@ on dpd.esh_id = du.esh_id_2016
 /*
 Author: Justine Schott
 Created On Date: 8/15/2016
-Last Modified Date: 12/29/2016
+Last Modified Date: 12/30/2016
 Name of QAing Analyst(s):
 Purpose: 2016 district data in terms of 2016 methodology with targeting assumptions built in
 Methodology:
