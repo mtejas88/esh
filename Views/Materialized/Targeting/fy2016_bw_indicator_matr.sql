4c83a37fa1802a26927468e7f9a3dcc58dc40aa7
jeremyholtzman <jeremyholtzman@gmail.com>
2017-01-30 15:34:25 -0800

    Changed 2015 to make sure it's clean
    @justineschott I edited the logic so that districts that were dirty
    can’t be “Not Targets”

--- Views/Materialized/Targeting/fy2016_bw_indicator_matr.sql
+++ Views/Materialized/Targeting/fy2016_bw_indicator_matr.sql
@@ -7,7 +7,7 @@ select
     when  d16.exclude_from_ia_analysis = false
           and d16.ia_bandwidth_per_student_kbps >= 100
             then 'Not Target'
-    when  d15.ia_bandwidth_per_student != 'Insufficient data' and d15.ia_bandwidth_per_student::numeric >= 100
+    when  d15.exclude_from_analysis = false and d15.ia_bandwidth_per_student::numeric >= 100
             then 'Not Target'
     when  d16.ia_applicants is null or d16.ia_applicants = ''
             then 'No Data'
@@ -27,7 +27,7 @@ where d16.include_in_universe_of_districts or d16.district_type = 'Charter'
 /*
 Author: Justine Schott
 Created On Date: 8/17/2016
-Last Modified Date: 11/22/2016
+Last Modified Date: 1/30/2017 - changed so that districts that are dirty in both years can't be Not Targets
 Name of QAing Analyst(s):
 Purpose: To identify districts' bandwidth target status
 Methodology: Compare 2015 and 2016 bw/student values, as well as whether they receive services
