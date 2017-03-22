b9bb8eb697b2cf63bc528e83ecda2224fb429d10
jeremyholtzman <jeremyholtzman@gmail.com>
2017-03-22 16:55:38 -0700

    just switched so that R can run the sql. already qa-ed by saptarshi

---
+++ Projects/Consultant Summary for Outreach/src/query_data.R
@@ -0,0 +1,58 @@
+# clear the console
+cat("\014")
+
+rm(list=ls())
+
+args = commandArgs(trailingOnly=TRUE)
+github_path <- args[1]
+
+## load packages (if not already in the environment)
+packages.to.install <- c("DBI", "rJava", "RJDBC", "dotenv")
+for (i in 1:length(packages.to.install)){
+  if (!packages.to.install[i] %in% rownames(installed.packages())){
+    install.packages(packages.to.install[i])
+  }
+}
+library(DBI)
+library(rJava)
+library(RJDBC)
+library(dotenv)
+
+#this is my github path. DONT FORGET TO COMMENT OUT
+#github_path <- '~/Documents/Analysis/ficher/'
+setwd(paste(github_path, 'Projects/Consultant Summary for Outreach', sep=''))
+
+
+options(java.parameters = "-Xmx4g" )
+
+## source environment variables
+source(paste(github_path, "General_Resources/common_functions/source_env.R", sep=""))
+source_env("~/.env")
+
+##**************************************************************************************************************************************************
+## QUERY THE DB
+
+
+## load PostgreSQL Driver
+pgsql <- JDBC("org.postgresql.Driver", paste(github_path, "General_Resources/postgres_driver/postgresql-9.4.1212.jre7.jar", sep=""), "`")
+
+## connect to the database
+con <- dbConnect(pgsql, url=url, user=user, password=password)
+
+## query function
+querydb <- function(query_name){
+  query <- readChar(query_name, file.info(query_name)$size)
+  data <- dbGetQuery(con, query)
+  return(data)
+}
+
+state <- querydb('src/consultant_summary_by_state.sql')
+national <- querydb('src/consultant_summary_national.sql')
+
+## disconnect from database
+dbDisconnect(con)
+
+##**************************************************************************************************************************************************
+
+write.csv(state, 'data/consultant_summary_by_state.csv', row.names = FALSE)
+write.csv(national, 'data/consultant_summary_national.csv', row.names = FALSE)
