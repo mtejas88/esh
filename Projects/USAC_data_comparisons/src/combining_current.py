d471501f6a80da8e61d552a6760f14b199dc4ff9
jeremyholtzman <jeremyholtzman@gmail.com>
2017-03-22 15:27:10 -0700

    adjusted the project stucture and the make file. Still need to work on the python file

--- Projects/USAC_data_comparisons/src/combining_current.py
+++ Projects/USAC_data_comparisons/src/combining_current.py
@@ -2,12 +2,16 @@ import pandas as pd
 import numpy as np
 import glob
 import csv
+import os
+
+#set working directory
+os.chdir('/Users/jeremyholtzman/Documents/Analysis/ficher/Projects/USAC_data_comparisons')
 
 frn_data = pd.DataFrame()
 recip_data = pd.DataFrame()
-#outputFile =open#"/Users/jeremyholtzman/Documents/Analysis/Current 2016 471s/combined_test.csv","w")
 
-files = glob.glob("/Users/jeremyholtzman/Documents/Analysis/ficher/Projects/USAC_data_comparisons/downloads/*Current_AL*.xlsx")
+files = glob.glob("*downloads/*Current_*.xlsx")
+
 
 for f in files:
 	print f
@@ -16,5 +20,6 @@ for f in files:
 	frn_data = frn_data.append(df,ignore_index=True)
 	recip_data = recip_data.append(df2,ignore_index=True)
 	
-frn_data.to_csv("/Users/jeremyholtzman/Documents/Analysis/ficher/Projects/USAC_data_comparisons/data/interim/all_frn.csv", encoding='utf-8')
-recip_data.to_csv("/Users/jeremyholtzman/Documents/Analysis/ficher/Projects/USAC_data_comparisons/data/interim/all_recip.csv", encoding='utf-8')
+print os.getcwd()
+#frn_data.to_csv("/Users/jeremyholtzman/Documents/Analysis/ficher/Projects/USAC_data_comparisons/data/interim/all_frn.csv", encoding='utf-8')
+#recip_data.to_csv("/Users/jeremyholtzman/Documents/Analysis/ficher/Projects/USAC_data_comparisons/data/interim/all_recip.csv", encoding='utf-8')
