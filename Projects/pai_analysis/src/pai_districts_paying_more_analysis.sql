01e0d82519b6c1dacc9bf9d5e096b2d5e0dc34e7
justineschott <justine@educationsuperhighway.org>
2017-03-29 12:45:31 -0700

    Update to aggregate analysis

--- Projects/pai_analysis/src/pai_districts_paying_more_analysis.sql
+++ Projects/pai_analysis/src/pai_districts_paying_more_analysis.sql
@@ -1,5 +1,5 @@
 with districts as (
-	select 
+	select
 		d16.district_size,
 		d16.locale,
 		d15.ia_monthly_cost*(1-d16.discount_rate_c1) as monthly_cost_2015,
@@ -11,7 +11,7 @@ with districts as (
 		case
 			when d15o.total_ia_bw_mbps > 0
 				then (d16.ia_bw_mbps_total - d15o.total_ia_bw_mbps)/
-						d15o.total_ia_bw_mbps 
+						d15o.total_ia_bw_mbps
 			else 1
 		end as bw_mbps_increase
 	from fy2016_districts_deluxe_matr d16
@@ -23,17 +23,53 @@ with districts as (
 	and d16.exclude_from_ia_cost_analysis = false
 	and d15.exclude_from_analysis = false
 	and d15.ia_monthly_cost > 0
+),
+
+cost_agg as (
+	select
+		locale,
+		case
+			when monthly_cost_increase > .1
+				then 'cost increase'
+			when monthly_cost_increase < -.1
+				then 'cost decrease'
+			else 'same cost'
+		end as cost_category,
+		count(*) as sample,
+		sum(case
+				when bw_mbps_increase > .1
+					then 1
+				else 0
+			end) as num_districts_bw_increase,
+		sum(case
+				when bw_mbps_increase < -.1
+					then 1
+				else 0
+			end) as num_districts_bw_decrease,
+		sum(case
+				when bw_mbps_increase >= -.1 and bw_mbps_increase <= .1
+					then 1
+				else 0
+			end) as num_districts_bw_flat,
+		median(bw_mbps_increase) as median_bw_mbps_increase,
+		sum(monthly_cost_2015)/1000000 as aggregate_monthly_cost_m_2015,
+		sum(monthly_cost_2016)/1000000 as aggregate_monthly_cost_m_2016,
+		sum(bw_mbps_2015)/1000 as aggregate_bw_gbps_2015,
+		sum(bw_mbps_2016)/1000 as aggregate_bw_gbps_2016,
+		median(bw_mbps_2016) as median_bw_mbps_2016
+	from districts
+	group by 1, 2
 )
 
-select 
-	case
-		when bw_mbps_increase > .1
-			then 'bw increase'
-		when bw_mbps_increase < -.1
-			then 'bw decrease'
-		else 'same bw'
-	end as bw_category,
-	count(*)
-from districts
-where monthly_cost_increase > .1
-group by 1
+select
+	locale,
+	cost_category,
+	num_districts_bw_increase/(num_districts_bw_increase+num_districts_bw_decrease+num_districts_bw_flat)::numeric as pct_districts_bw_increase,
+	num_districts_bw_decrease/(num_districts_bw_increase+num_districts_bw_decrease+num_districts_bw_flat)::numeric as pct_districts_bw_decrease,
+	num_districts_bw_flat/(num_districts_bw_increase+num_districts_bw_decrease+num_districts_bw_flat)::numeric as pct_districts_bw_flat,
+	(aggregate_monthly_cost_m_2016 - aggregate_monthly_cost_m_2015)/aggregate_monthly_cost_m_2015 as pct_increase_monthly_cost,
+	(aggregate_bw_gbps_2016 - aggregate_bw_gbps_2015)/aggregate_bw_gbps_2015 as pct_increase_bw,
+	median_bw_mbps_increase,
+	median_bw_mbps_2016,
+	sample
+from cost_agg
