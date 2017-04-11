adb4d5a1117158f1eb6b7ebb6cff23c1651942e5
justineschott <justine@educationsuperhighway.org>
2017-04-11 16:43:35 -0700

    Cost/mbps and bw/student
    digging into trends

---
+++ Projects/special_construction/src/features/num_bids_cost_per_mbps.R
@@ -0,0 +1,48 @@
+## ==============================================
+##
+## BIDS ANALYSIS - COST PER MBPS
+## 
+## OBJECTIVES:
+##    -- Compare cost per mbps by number of bids 
+##    -- Dig into trends 
+##
+## ==============================================
+
+## Clearing memory
+rm(list=ls())
+## setting working directory
+setwd("C:/Users/Justine/Documents/GitHub/ficher/Projects/special_construction/")
+
+library(lubridate)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(ggmap)
+library(RColorBrewer)
+library(scales)
+library(gridExtra)
+
+##bids prepping
+bids <- read.csv("C:/Users/Justine/Documents/GitHub/ficher/Projects/special_construction/data/external/frns_with_district_info.csv", 
+                 as.is=TRUE)
+bids <- filter(bids, bids$num_bids_received < 26)
+bids$indic_0_bids <- ifelse(bids$num_bids_received == 0,1,0)
+bids$indic_1_bids <- ifelse(bids$num_bids_received == 1,1,0)
+bids$indic_2p_bids <- ifelse(bids$num_bids_received > 1,1,0)
+bids$num_bids_category <- ifelse(bids$num_bids_received > 1,2,bids$num_bids_received)
+bids$urban_indicator <- ifelse(bids$locale == 'Urban', 'true', 'false')
+bids$suburban_indicator <- ifelse(bids$locale == 'Suburban', 'true', 'false')
+bids$town_indicator <- ifelse(bids$locale == 'Town', 'true', 'false')
+bids$rural_indicator <- ifelse(bids$locale == 'Rural', 'true', 'false')
+
+bids_clean <- filter(bids, bids$exclude_from_ia_cost_analysis == 'false')
+bids_clean <- filter(bids_clean, bids_clean$exclude_from_ia_analysis == 'false')
+
+groups_bids_clean <- group_by(bids_clean, fiber_target_status, locale, num_bids_category)
+summary <- summarize(groups_bids_clean, 
+                    count = n(), 
+                    median_ia_monthly_cost_per_mbps = median(ia_monthly_cost_per_mbps, na.rm = T), 
+                    median_ia_bandwidth_per_student_kbps = median(ia_bandwidth_per_student_kbps, na.rm = T))
+
+write.csv(summary, file = "data/interim/summary.csv")
+
