e32bd9018a355dcde7f457c5dcddf0bd5f8ca3fa
justineschott <justine@educationsuperhighway.org>
2017-05-17 17:27:52 -0700

    Updates
    aggregating funding and eligible counts

--- Projects/caf/caf_funded.sql
+++ Projects/caf/caf_funded.sql
@@ -1,44 +1,44 @@
-/*select *
-from (*/
-  select
-    sum(final_funding) as caf_p1_funds,
-    sum(esh_calculated_annual_a_cam_support)*10 as ror_funds,
-    count(*) as num_blocks
-  from caf.caf_p1_funds
-  full outer join (
-    select ror_funded_blocks.*, annual_a_cam_support::numeric, authorization_date, holding_company,
-    number_of_locations_in_eligible_cbs_obliged_to_offer_10_1_mbps, number_of_locations_in_eligible_cbs_obliged_to_offer_25_3_mbps,
-    number_of_locations_in_eligible_cbs_obliged_to_offer_4_1_mbps, number_of_locations_remaining_on_reasonable_request_standard,
-    rate_of_return_carrier, ror_r2_authorizations.state, total_ror_locations_in_cbs_receiving_model_based_funding,
-    annual_a_cam_support::numeric * supported_locations_232 / total_ror_locations_in_cbs_receiving_model_based_funding
-      as esh_calculated_annual_a_cam_support
+with caf_p1_fmt as (
+	select 	fips as census_block,
+			sum(final_funding) as p1_funding_authorized,
+			array_agg(distinct carrier) as p1_carriers
+	from caf.caf_p1_funds
+	group by 1
+),
+ror_fmt as (
+    select ror_funded_blocks.census_block,
+    sum(annual_a_cam_support::numeric * supported_locations_232 / total_ror_locations_in_cbs_receiving_model_based_funding)
+    	as ror_funding_authorized_assumed,
+    array_agg(distinct holding_company) as ror_carriers
     from caf.ror_funded_blocks
     left join caf.ror_r2_authorizations
     on trim(both ' ' from ror_funded_blocks.short_name) = trim(both ' ' from ror_r2_authorizations.rate_of_return_carrier)
     and ror_funded_blocks.state = ror_r2_authorizations.state
-  ) esh_ror_funded_blocks_r2_authorizations
-  on caf_p1_funds.fips = esh_ror_funded_blocks_r2_authorizations.census_block/*) j1
-full outer join
-on  case
-      when j1.fips is null
-        then j1.census_block
-      else j1.fips
-    end = .*/
+    group by 1
+),
+j1 as (
+	select 	case
+				when p1.census_block is null
+					then ro.census_block
+				else p1.census_block
+			end as census_block,
+			p1_funding_authorized,
+			ror_funding_authorized_assumed,
+			p1_carriers,
+			ror_carriers
+	from caf_p1_fmt p1
+	full outer join ror_fmt ro
+	on p1.census_block = ro.census_block
+)
 
-/*
-caf.ny_auction_blocks
-caf.challenge
-caf.acam232_blocks
-caf.ror_authorizations
-full outer join caf.frontier_blocks
-full outer join caf.cam43_es_high_cost
-full outer join caf.centurylink_blocks
-full outer join caf.frontier_r2_blocks
-full outer join caf.pc_eligible_blocks
-full outer join caf.rbe_eligible_blocks
-full outer join caf.cam43_eligible_blocks
-caf.ror_r2_eligible_blocks
-caf.ror_r1_eligible_blocks
-full outer join caf.pc_ph2_eligible_blocks
-full outer join caf.summary_authorizations
-full outer join caf.ror_several_authorizations */
+select
+	case
+		when p1_funding_authorized > 0 and ror_funding_authorized_assumed > 0
+			then 'overlap'
+		else 'no overlap'
+	end as block_type,
+	count(*) as census_blocks,
+	sum(p1_funding_authorized) as p1_funding_authorized,
+	sum(ror_funding_authorized_assumed) as ror_funding_authorized_assumed
+from j1
+group by 1
