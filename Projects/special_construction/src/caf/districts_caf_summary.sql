a53ccdba60144b6d53e0f844dd9d404178270b79
justineschott <justine@educationsuperhighway.org>
2017-05-31 14:33:04 -0700

    funded blocks
    split by fiber target status

--- Projects/special_construction/src/caf/districts_caf_summary.sql
+++ Projects/special_construction/src/caf/districts_caf_summary.sql
@@ -1,6 +1,7 @@
 with dists as (
   select
     district_esh_id,
+    fiber_target_status,
     count(distinct  case
                       when census_block_eligible
                         then blockcode
@@ -10,11 +11,14 @@ with dists as (
                         then blockcode
                     end) as funded_census_blocks,
     count(*) as census_blocks
-  from districts_caf
-  group by 1
+  from districts_caf d
+  left join fy2016_districts_deluxe_matr dd
+  on d.district_esh_id = dd.esh_id
+  group by 1, 2
 )
 
 select
+  'all' as fiber_target_status,
   funded_census_blocks > 0 as funded_census_blocks,
   eligible_census_blocks > 0 as eligible_census_blocks,
   median(census_blocks) as median_census_blocks,
@@ -22,4 +26,17 @@ select
   median(funded_census_blocks/census_blocks::numeric) as median_pct_funded_census_blocks,
   count(*)
 from dists
-group by 1, 2
+group by 2, 3
+
+UNION
+
+select
+  fiber_target_status,
+  funded_census_blocks > 0 as funded_census_blocks,
+  eligible_census_blocks > 0 as eligible_census_blocks,
+  median(census_blocks) as median_census_blocks,
+  median(eligible_census_blocks/census_blocks::numeric) as median_pct_eligible_census_blocks,
+  median(funded_census_blocks/census_blocks::numeric) as median_pct_funded_census_blocks,
+  count(*)
+from dists
+group by 1, 2, 3
