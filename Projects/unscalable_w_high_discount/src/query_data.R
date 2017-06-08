6ea368206ce82f5133072662663fd1b7188f6e77
jeremyholtzman <jeremyholtzman@gmail.com>
2017-06-07 17:42:06 -0700

    unscalable campuses by discount rate

---
+++ Projects/unscalable_w_high_discount/src/query_data.R
@@ -0,0 +1,52 @@
+## =========================================
+##
+## QUERY DATA FROM THE DB
+##
+## =========================================
+
+## Clearing memory
+rm(list=ls())
+
+## load packages (if not already in the environment)
+packages.to.install <- c("DBI", "rJava", "RJDBC", "dotenv")
+for (i in 1:length(packages.to.install)){
+  if (!packages.to.install[i] %in% rownames(installed.packages())){
+    install.packages(packages.to.install[i])
+  }
+}
+library(DBI)
+library(rJava)
+library(RJDBC)
+library(dotenv)
+options(java.parameters = "-Xmx4g" )
+
+## source environment variables
+source("../../General_Resources/common_functions/source_env.R")
+source_env("~/.env")
+
+
+##**************************************************************************************************************************************************
+## QUERY THE DB
+
+## load PostgreSQL Driver
+pgsql <- JDBC("org.postgresql.Driver", paste(github_path, "General_Resources/postgres_driver/postgresql-9.4.1212.jre7.jar", sep=""), "`")
+
+## connect to the database
+con <- dbConnect(pgsql, url=url, user=user, password=password)
+
+## query function
+querydb <- function(query_name){
+  query <- readChar(query_name, file.info(query_name)$size)
+  data <- dbGetQuery(con, query)
+  return(data)
+}
+
+unscalable.dd <- querydb("src/unscalable_discount.SQL")
+
+## disconnect from database
+dbDisconnect(con)
+
+##**************************************************************************************************************************************************
+## write out the datasets
+
+write.csv(unscalable.dd, "data/raw/unscalable_districts.csv", row.names=F)
