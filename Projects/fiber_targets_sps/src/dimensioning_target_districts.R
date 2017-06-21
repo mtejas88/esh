e731fa3052a115062299e424c46108cdd6567754
Justine <justine@educationsuperhighway.org>
2017-06-21 19:44:46 +0200

    Adding in universe breakdowns

--- Projects/fiber_targets_sps/src/dimensioning_target_districts.R
+++ Projects/fiber_targets_sps/src/dimensioning_target_districts.R
@@ -10,52 +10,102 @@
 rm(list=ls())
 
 ## Setting working directory -- needs to be changed
-setwd("C:/Users/jesch/OneDrive/Documents/GitHub/ficher/Projects/high_cost_profiling")
+setwd("C:/Users/jesch/OneDrive/Documents/GitHub/ficher/Projects/fiber_targets_sps")
 
 ##load packages
 library(dplyr)
 library(ggplot2)
 
 ##**************************************************************************************************************************************************
-## READ IN DATA
-
+## Read in data
 target.districts <- read.csv("data/raw/characteristics_of_target_districts.csv", as.is=T, header=T, stringsAsFactors=F)
+districts <- read.csv("data/raw/characteristics_of_districts.csv", as.is=T, header=T, stringsAsFactors=F)
 
-num.districts <- unique(target.districts$district_esh_id) %>% length()
-print(paste0('There are ', num.districts, ' fiber target districts'))
+target.districts.count <- nrow(target.districts)
+districts.count <- nrow(districts)
 
-#Dimension by locale
+#Dimension by locale - fiber targets
 locale.target.districts <- group_by(target.districts, locale) %>%
-  summarise(num.districts = n())
+  summarise(num.districts = n(),
+            pct.districts = num.districts/target.districts.count)
 
-pdf('figures/locale.pdf', width = 7, height = 5)
-ggplot(locale.target.districts, aes(x = locale, y = num.districts)) + 
+png('figures/locale.png')
+ggplot(locale.target.districts, aes(x = locale, y = pct.districts)) + 
   geom_bar(stat = 'identity', fill = '#fdb913') + 
   ggtitle("Fiber target districts by locale") +
-  labs(x = 'Locale', y = 'Number of Districts') +
+  labs(x = 'Locale', y = '% Districts') +
+  ylim(0, 1) +
   theme_grey()
 dev.off()
 
-#Dimension by size
-size.target.districts <- group_by(target.districts, district_size) %>%
-  summarise(num.districts = n())
+#Dimension by locale - all
+locale.districts <- group_by(districts, locale) %>%
+  summarise(num.districts = n(),
+            pct.districts = num.districts/districts.count)
 
-pdf('figures/district_size.pdf', width = 7, height = 5)
-ggplot(size.target.districts, aes(x = district_size, y = num.districts)) + 
+png('figures/locale_all.png')
+ggplot(locale.districts, aes(x = locale, y = pct.districts)) + 
+  geom_bar(stat = 'identity', fill = '#009296') + 
+  ggtitle("Districts by locale") +
+  labs(x = 'Locale', y = '% Districts')+ 
+  ylim(0, 1) +
+  theme_grey()
+dev.off()
+
+#Dimension by size - fiber targets
+size.target.districts <- group_by(target.districts, district_size) %>%
+  summarise(num.districts = n(),
+            pct.districts = num.districts/target.districts.count)
+
+png('figures/district_size.png')
+ggplot(size.target.districts, aes(x = district_size, y = pct.districts)) + 
   geom_bar(stat = 'identity', fill = '#fdb913') + 
   ggtitle("Fiber target districts by district size") +
-  labs(x = 'District size', y = 'Number of Districts') +
+  labs(x = 'District size', y = '% Districts') +
+  ylim(0, 1) +
   theme_grey()
 dev.off()
 
-#Dimension by number of fiber providers in 477
-providers.target.districts <- group_by(target.districts, nproviders) %>%
-  summarise(num.districts = n())
+#Dimension by size - all
+size.districts <- group_by(districts, district_size) %>%
+  summarise(num.districts = n(),
+            pct.districts = num.districts/districts.count)
 
-pdf('figures/fiber_providers.pdf', width = 7, height = 5)
-ggplot(providers.target.districts, aes(x = nproviders, y = num.districts)) + 
+png('figures/district_size_all.png')
+ggplot(size.districts, aes(x = district_size, y = pct.districts)) + 
+  geom_bar(stat = 'identity', fill = '#009296') + 
+  ggtitle("Districts by district size") +
+  labs(x = 'District size', y = '% Districts') +
+  ylim(0, 1) +
+  theme_grey()
+dev.off()
+
+
+#Dimension by number of fiber providers in 477 - fiber targets
+providers.target.districts <- group_by(target.districts, nproviders) %>%
+  summarise(num.districts = n(),
+            pct.districts = num.districts/target.districts.count)
+
+png('figures/fiber_providers.png')
+ggplot(providers.target.districts, aes(x = nproviders, y = pct.districts)) + 
   geom_bar(stat = 'identity', fill = '#fdb913') + 
   ggtitle("Fiber target districts by number of fiber providers in 477") +
-  labs(x = 'Number of Fiber Providers', y = 'Number of Districts') +
+  labs(x = 'Number of Fiber Providers', y = '% Districts') +
+  ylim(0, 1) +
   theme_grey()
 dev.off()
+
+#Dimension by number of fiber providers in 477 - all
+providers.districts <- group_by(districts, nproviders) %>%
+  summarise(num.districts = n(),
+            pct.districts = num.districts/districts.count)
+
+png('figures/fiber_providers_all.png')
+ggplot(providers.districts, aes(x = nproviders, y = pct.districts)) + 
+  geom_bar(stat = 'identity', fill = '#009296') + 
+  ggtitle("Districts by number of fiber providers in 477") +
+  labs(x = 'Number of Fiber Providers', y = '% Districts') +
+  ylim(0, 1) +
+  theme_grey()
+dev.off()
+
