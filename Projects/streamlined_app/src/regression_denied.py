9b4beadcfe3c95748fc2ae8654396b9905bf6216
Justine <justine@educationsuperhighway.org>
2017-06-27 18:13:26 +0300

    Additional variables

--- Projects/streamlined_app/src/regression_denied.py
+++ Projects/streamlined_app/src/regression_denied.py
@@ -17,10 +17,10 @@ applications['discount_category'] = np.floor(applications['category_one_discount
 lowcost_applications  = applications.loc[applications['frns'] == applications['funded_frns'] + applications['denied_frns']]
 lowcost_applications  = lowcost_applications.loc[lowcost_applications['lowcost_indicator'] == True]
 
-lowcost_applications['denied_indicator'] = np.where(lowcost_applications['denied_frns'] > 0, True, False)
-lowcost_applications['no_consultant_indicator'] = np.where(lowcost_applications['consultant_indicator'] == False, True, False)
+lowcost_applications['denied_indicator'] = np.where(lowcost_applications['denied_frns'] > 0, 1, 0)
+lowcost_applications['no_consultant_indicator'] = np.where(lowcost_applications['consultant_indicator'] == False, 1, 0)
 
-lowcost_applications['services_2p_indicator'] = np.where(lowcost_applications['num_service_types'] == 1, False, True)
+lowcost_applications['services_2p_indicator'] = np.where(lowcost_applications['num_service_types'] == 1, 0, 1)
 
 den = lowcost_applications.groupby('denied_indicator')
 print(den.agg({'application_number': lambda x: x.count(),  'total_funding_year_commitment_amount_request':'sum'}))
@@ -38,18 +38,23 @@ category_dummies = pd.get_dummies(lowcost_applications.category_of_service, pref
 lowcost_applications = pd.concat([lowcost_applications, category_dummies], axis=1)
 
 
-feature_cols = ['no_consultant_indicator', 'special_construction_indicator', 'services_2p_indicator', 'applicant_type_School', 'locale_Urban', 'category_2']
+feature_cols = ['no_consultant_indicator', 'special_construction_indicator', 'services_2p_indicator', 'applicant_type_School', 'locale_Urban', 'category_2', 'fulltime_enrollment','num_recipients', 'category_one_discount_rate', 'applicant_type_School District','applicant_type_Consortium','applicant_type_Library']
 
-insig_cols = ['num_recipients', 'applicant_type_Consortium', 'applicant_type_Library', 'applicant_type_School District', 'category_one_discount_rate', 'fulltime_enrollment']
+insig_cols = []
 
 X = lowcost_applications[feature_cols]
 y = lowcost_applications.denied_indicator
 
 # regression model
 X = sm.add_constant(X)
-est = sm.OLS(y, X.astype(float)).fit()
+est = sm.Logit(y, X.astype(float)).fit()
 print(est.summary())
 
 ##how many apps with worst case were denied funding
 wor = lowcost_applications.groupby(['no_consultant_indicator', 'special_construction_indicator', 'services_2p_indicator', 'applicant_type_School', 'locale_Urban', 'category_2'])
 print(wor.agg({'application_number': lambda x: x.count()}))
+
+## histograms of all variables
+hist_cols = ['no_consultant_indicator', 'special_construction_indicator', 'services_2p_indicator', 'applicant_type_School', 'locale_Urban', 'category_2', 'denied_indicator']
+lowcost_applications[hist_cols].hist()
+pylab.show()
