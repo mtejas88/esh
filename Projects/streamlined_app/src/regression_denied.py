f998b1b8867839f8408e89f3bbe0a9f8db7584b0
Justine <justine@educationsuperhighway.org>
2017-06-30 02:44:15 +0300

    random under sampling
    since the error rate is now high for will also compare to exact logistic regression using R

--- Projects/streamlined_app/src/regression_denied.py
+++ Projects/streamlined_app/src/regression_denied.py
@@ -9,6 +9,11 @@ import pandas as pd
 import numpy as np
 import scipy
 import statsmodels.api as sm
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+import pylab as pl
+from imblearn.under_sampling import RandomUnderSampler
+import warnings
 
 ## data prep
 #import
@@ -43,10 +48,45 @@ insig_cols = ['applicant_type_School District','applicant_type_Consortium','appl
 X = lowcost_applications[feature_cols]
 y = lowcost_applications.denied_indicator
 
+
 # regression model
 X = sm.add_constant(X)
 est = sm.Logit(y, X.astype(float)).fit()
 print(est.summary())
+
+##regression with test/train
+train, test = train_test_split(lowcost_applications, train_size=0.75, random_state=1)
+
+X1 = train[feature_cols]
+y1 = train.denied_indicator
+
+# regression model
+rus = RandomUnderSampler(random_state=1)
+X_res, y_res = rus.fit_sample(X1,y1)
+rows = X_res.shape[0]
+
+X_res =  pd.DataFrame(data=X_res, index=range(rows), columns=feature_cols)
+y_res =  pd.DataFrame(data=y_res, index=range(rows), columns=['denied_indicator'])
+
+X_res = sm.add_constant(X_res)
+
+est1 = sm.Logit(y_res, X_res.astype(float)).fit()
+print(est1.summary())
+
+# predicting
+x_test = test[feature_cols]
+x_test = sm.add_constant(x_test)
+
+y_test = test.denied_indicator
+
+yhat_test = est1.predict(x_test)
+plt.hist(yhat_test,50)
+plt.show()
+
+yhat_test = [ 0 if y < 0.35 else 1 for y in yhat_test ]
+
+print(confusion_matrix(y_test, yhat_test))
+print(classification_report(y_test, yhat_test,digits=3))
 
 ##how many apps with worst case were denied funding
 wor = lowcost_applications.groupby(['no_consultant_indicator', 'special_construction_indicator', 'services_2p_indicator', 'applicant_type_School', 'locale_Urban', 'category_2'])
