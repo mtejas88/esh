3a9f4913c96674c2746e0289f2a878ad47976e07
Sierra Costanza <sierra@Sierras-MacBook-Pro.local>
2017-07-12 08:17:08 -0700

    updating outlier methodology to catch more outliers via expanding 2016 data (Chris' idea)

--- Projects/outlier_detection/scripts/03_use_cases.R
+++ Projects/outlier_detection/scripts/03_use_cases.R
@@ -44,39 +44,39 @@ identify_outliers_loop <- function(o_16,output,significance_level,cost_column,un
       #if-else conditions below to catch cases when n_17_at_a_time isn't a perfect interval to cover all points
       if (i+n_17_at_time < nrow(output)) {
         outliers=rbind(outliers,
-      identify_outliers(output[c(1:frm,(i+1):(i+n_17_at_time)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
+                       identify_outliers(output[c(1:frm,(i+1):(i+n_17_at_time)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
       }
       else if ((i+n_17_at_time) == nrow(output)) {
         outliers=rbind(outliers,
-      identify_outliers(output[c(1:frm,nrow(output)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
+                       identify_outliers(output[c(1:frm,nrow(output)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
       }
       else if ((i < nrow(output)) & (i+n_17_at_time > nrow(output))) {
         outliers=rbind(outliers,
-        identify_outliers(output[c(1:o_16,(i+1):nrow(output)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
+                       identify_outliers(output[c(1:o_16,(i+1):nrow(output)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
       }  
     }
   }
   #2.b. if we are NOT removing the 2016 outliers first (keeping 2016 data intact)
   else {
     if (n_17_at_time > 0) {
-    #the starting index for the 2017 data is o_16 (=size of 2016 filtered data, including outliers) 
-    for(i in seq(from=o_16, to=(nrow(output) + n_17_at_time), by=n_17_at_time)){
-      #if-else conditions below to catch cases when n_17_at_a_time isn't a perfect interval to cover all points
-      if (i+n_17_at_time < nrow(output)) {
-        outliers=rbind(outliers,
-        identify_outliers(output[c(1:o_16,(i+1):(i+n_17_at_time)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
-      }     
-      else if ((i+n_17_at_time) == nrow(output)) {
-        outliers=rbind(outliers,
-        identify_outliers(output[c(1:o_16,nrow(output)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
-      }    
-      else if ((i < nrow(output)) & (i+n_17_at_time > nrow(output))) {
-        outliers=rbind(outliers,
-        identify_outliers(output[c(1:o_16,(i+1):nrow(output)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
-      }  
-    } 
+      #the starting index for the 2017 data is o_16 (=size of 2016 filtered data, including outliers) 
+      for(i in seq(from=o_16, to=(nrow(output) + n_17_at_time), by=n_17_at_time)){
+        #if-else conditions below to catch cases when n_17_at_a_time isn't a perfect interval to cover all points
+        if (i+n_17_at_time < nrow(output)) {
+          outliers=rbind(outliers,
+                         identify_outliers(output[c(1:o_16,(i+1):(i+n_17_at_time)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
+        }     
+        else if ((i+n_17_at_time) == nrow(output)) {
+          outliers=rbind(outliers,
+                         identify_outliers(output[c(1:o_16,nrow(output)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
+        }    
+        else if ((i < nrow(output)) & (i+n_17_at_time > nrow(output))) {
+          outliers=rbind(outliers,
+                         identify_outliers(output[c(1:o_16,(i+1):nrow(output)),],significance_level,cost_column,unique_id,use_case_name,with_16,n_17_at_time ))
+        }  
+      } 
     } else {output16=outliers_2016
-      return(output16)} #for if we are just identifying 2016 data outliers
+    return(output16)} #for if we are just identifying 2016 data outliers
   }
   #3. append to master output
   master_output=rbind(master_output,outliers)
@@ -104,57 +104,120 @@ use_case_li <- function (data, data_17, metric, circuit_size, technology, line_i
   o_16=nrow(output16)
   #filter for 2017 data use cases - have to separately because they need to be stacked in order to construct the loop
   if (dim(data_17)[1] > 1) {
-  data_17=as.data.frame(data_17)
-  data_17["year"]=2017
-  output17 <- data_17 %>%
-    filter(
-      round(bandwidth_in_mbps,digits=1) %in% circuit_size,
-      connect_category %in% technology,
-      purpose %in% line_item_purpose
-    ) 
-  #combine filtered outputs
-  output=rbind(output16,output17) }
+    data_17=as.data.frame(data_17)
+    data_17["year"]=2017
+    output17 <- data_17 %>%
+      filter(
+        round(bandwidth_in_mbps,digits=1) %in% circuit_size,
+        connect_category %in% technology,
+        purpose %in% line_item_purpose
+      ) 
+    #combine filtered outputs
+    output=rbind(output16,output17) }
   else {
     output=output16  
   }
-str1 <- paste0("\"circuit_size_in_mbps\" => \"", circuit_size,"\"")
-str2 <- paste0("\"technology\" => \"", technology,  "\"")
-str3 <- paste0("\"purpose\" => \"", line_item_purpose, "\"")
-
-use_case_parameters <- paste(str1, str2, str3, sep = ", ")
-output <- cbind(use_case_parameters, output)
-
-#define the column based on input metric - will modify this as new use cases come up
-if (metric=="Cost per Circuit"){
-  column='monthly_cost_per_circuit'
-}
-
-outputlist=identify_outliers_loop(o_16,output,0.05,column, "line_item_id",metric,with_16,n_17_at_time)
-
-#combine 2016 "normal data" distribution and 2017 outliers in order to visualize
-distribution=outputlist$base_2016[,c("line_item_id",column)]
-distribution[,"metric"]=metric
-names(distribution)=c("outlier_unique_id", "outlier_value","outlier_use_case_name")
-distribution[,"outlier_flag"]=0
-
-outliers_2017=as.data.frame(outputlist$outliers_2017)
-if (nrow(outliers_2017) > 0) {
-  outliers_2017[,"outlier_flag"]=1
-  distribution=rbind(distribution,outliers_2017)
-}
-#add in separate columns for the parameters 
-distribution[,"bandwidth_in_mbps"]=circuit_size
-distribution[,"connect_category"]=technology
-distribution[,"purpose"]=line_item_purpose
-distribution[,"id"]=c(1:nrow(distribution))
-
-return(distribution)
+  str1 <- paste0("\"circuit_size_in_mbps\" => \"", circuit_size,"\"")
+  str2 <- paste0("\"technology\" => \"", technology,  "\"")
+  str3 <- paste0("\"purpose\" => \"", line_item_purpose, "\"")
+  
+  use_case_parameters <- paste(str1, str2, str3, sep = ", ")
+  output <- cbind(use_case_parameters, output)
+  
+  #define the column based on input metric - will modify this as new use cases come up
+  if (metric=="Cost per Circuit"){
+    column='monthly_cost_per_circuit'
+  }
+  
+  outputlist=identify_outliers_loop(o_16,output,0.05,column, "line_item_id",metric,with_16,n_17_at_time)
+  
+  #combine 2016 "normal data" distribution and 2017 outliers in order to visualize
+  distribution=outputlist$base_2016[,c("line_item_id",column)]
+  distribution[,"metric"]=metric
+  names(distribution)=c("outlier_unique_id", "outlier_value","outlier_use_case_name")
+  distribution[,"outlier_flag"]=0
+  
+  outliers_2017=as.data.frame(outputlist$outliers_2017)
+  if (nrow(outliers_2017) > 0) {
+    outliers_2017[,"outlier_flag"]=1
+    distribution=rbind(distribution,outliers_2017)
+  }
+  #add in separate columns for the parameters 
+  distribution[,"bandwidth_in_mbps"]=circuit_size
+  distribution[,"connect_category"]=technology
+  distribution[,"purpose"]=line_item_purpose
+  distribution[,"id"]=c(1:nrow(distribution))
+  
+  return(distribution)
 }
 
 ###
 # use case type #2
 # identify outliers based on district-level data, state and national level
 ###
+
+##function for optimized locale and size filtering
+filter_opt=function(n=-1,data,state,district_locale,size,column) {
+  locales=c('Rural','Town','Suburban','Urban')
+  sizes=c('Tiny','Small','Medium','Large','Mega')
+  l_ind=match(district_locale,locales)
+  s_ind=match(size,sizes)
+  es=0
+  base_filter=function(data,state,district_locale,size,column) {
+    if (state!='National') {
+      output16 <- data %>%
+        filter(
+          postal_cd %in% state,
+          locale %in% district_locale,
+          district_size %in% size)  %>% 
+        filter_(paste('!is.na(', column, ')'))
+    } else {
+      output16 <- data %>%
+        filter(
+          locale %in% district_locale,
+          district_size %in% size) %>% 
+        filter_(paste('!is.na(', column, ')'))
+    }
+    return(output16)
+  }
+  while(n <= 25) {
+    if(n==-1) {
+      output16=base_filter(data,state,district_locale,size,column)
+      n=ifelse(nrow(output16) >= 0, nrow(output16), 0)
+    }else{
+      if(district_locale !='Urban') {
+        for (l in l_ind:length(locales)) {
+          locales_check=locales[l_ind:l]
+          output16=base_filter(data,state,locales_check,size,column)
+          n=nrow(output16)
+          if(n > 25){
+            break
+          }
+        }
+      }
+      for (s in s_ind:length(sizes)) {
+        if(n > 25){
+          break
+        }
+        sizes_check=sizes[s_ind:s]
+        if (exists("locales_check")) {
+          output16=base_filter(data,state,locales_check,sizes_check,column)}
+        else{
+          output16=base_filter(data,state,district_locale,sizes_check,column)}
+        n=nrow(output16)
+        es=s
+        if(n > 25){
+          break
+        }
+      }
+    }
+    if(es == length(sizes) & n <= 25){
+      return(base_filter(data,state,district_locale,size,column))
+      break
+    }
+  }
+  return(output16)
+} 
 
 use_case_district <- function (data, data_17, metric, district_locale, size, state, with_16,n_17_at_time) {
   #column that flags the year in both the 2016 and 2017 data
@@ -168,54 +231,41 @@ use_case_district <- function (data, data_17, metric, district_locale, size, sta
   } else if (metric=="Change in Total Monthly Cost") {
     column='change_in_cost_tot'
   } else if (metric=="% Change in Monthly Cost") {
-  column='change_in_cost_pct'
+    column='change_in_cost_pct'
   } else if (metric=="Monthly Cost per Mbps") {
     column='ia_monthly_cost_per_mbps'
   } else if (metric=="BW per Student") {
     column='ia_bandwidth_per_student_kbps'
   }
-
+  
   #filter for 2016 data use cases
-  if (state!='National') {
-    output16 <- data %>%
-      filter(
-        postal_cd %in% state,
-        locale %in% district_locale,
-        district_size %in% size)  %>% 
-    filter_(paste('!is.na(', column, ')'))
-    } else {
-    output16 <- data %>%
-      filter(
-        locale %in% district_locale,
-        district_size %in% size) %>% 
-      filter_(paste('!is.na(', column, ')'))
-    }
+  output16=filter_opt(n=-1,data,state,district_locale,size,column)
   o_16=nrow(output16)
   #sample size check
   if (o_16 >= 25) {
     #filter for 2017 data use cases - have to separately because they need to be stacked in order to construct the loop
     if (dim(data_17)[1] > 1) {
-    data_17=as.data.frame(data_17)
-    data_17["year"]=2017
-    if (state!='National') {
-      output17 <- data_17 %>%
-        filter(
-          postal_cd %in% state,
-          locale %in% district_locale,
-          district_size %in% size) %>% 
-        filter_(paste('!is.na(', column, ')'))
+      data_17=as.data.frame(data_17)
+      data_17["year"]=2017
+      if (state!='National') {
+        output17 <- data_17 %>%
+          filter(
+            postal_cd %in% state,
+            locale %in% district_locale,
+            district_size %in% size) %>% 
+          filter_(paste('!is.na(', column, ')'))
       } else {
-          output17 <- data_17 %>%
-            filter(
-              locale %in% district_locale,
-              district_size %in% size) %>% 
-            filter_(paste('!is.na(', column, ')'))
-        }
-    #combine filtered outputs
-    output=rbind(output16,output17) }
-      else {
-      output=output16  
+        output17 <- data_17 %>%
+          filter(
+            locale %in% district_locale,
+            district_size %in% size) %>% 
+          filter_(paste('!is.na(', column, ')'))
       }
+      #combine filtered outputs
+      output=rbind(output16,output17) }
+    else {
+      output=output16  
+    }
     str1 <- paste0("\"district_locale\" => \"", district_locale,"\"")
     str2 <- paste0("\"district_size\" => \"", size,"\"")
     str3 <- paste0("\"district_state\" => \"", state,"\"")
@@ -231,8 +281,8 @@ use_case_district <- function (data, data_17, metric, district_locale, size, sta
     
     outliers_2017=as.data.frame(outputlist$outliers_2017)
     if (nrow(outliers_2017) > 0) {
-    outliers_2017[,"outlier_flag"]=1
-    distribution=rbind(distribution,outliers_2017)
+      outliers_2017[,"outlier_flag"]=1
+      distribution=rbind(distribution,outliers_2017)
     }
     
     #add in separate columns for the parameters 
@@ -243,5 +293,5 @@ use_case_district <- function (data, data_17, metric, district_locale, size, sta
   } else {
     distribution=NULL
   }
-return(distribution)
+  return(distribution)
 }
