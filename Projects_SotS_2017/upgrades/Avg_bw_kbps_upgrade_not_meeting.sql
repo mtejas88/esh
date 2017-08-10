483d7e491cb3615cfe954e630f6b977ddba48412
Saaim <saaim.aslam@educationsuperhighway.org>
2017-08-10 07:49:17 -0700

    avg bw kbps for districts upgraded but not meeting 2014 goal

---
+++ Projects_SotS_2017/upgrades/Avg_bw_kbps_upgrade_not_meeting.sql
@@ -0,0 +1,9 @@
+select sum(ia_bw_mbps_total) as total_ia, count(esh_id) as total_esh,
+sum(num_students) as total_students,
+((sum(ia_bw_mbps_total))/(sum(num_students)))*1000 as average_ia_kbps_student --2007
+from public.fy2017_districts_deluxe_matr
+where
+include_in_universe_of_districts = 'True'
+and district_type = 'Traditional'
+and upgrade_indicator = 'True'
+and meeting_2014_goal_no_oversub = 'False'
