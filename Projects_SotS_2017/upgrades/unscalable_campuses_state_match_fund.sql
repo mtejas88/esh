2cdec2399b78c794a143a2c70285d3dd88a7b14c
Saaim <saaim.aslam@educationsuperhighway.org>
2017-08-12 17:59:35 -0700

    adding unscalable in state match fund, adding 2017 & 2016 upgraders demog

--- Projects_SotS_2017/upgrades/unscalable_campuses_state_match_fund.sql
+++ Projects_SotS_2017/upgrades/unscalable_campuses_state_match_fund.sql
@@ -1,12 +1,7 @@
-
 select
-sum(sots_known_unscalable_campuses+sots_assumed_unscalable_campuses) as total_campuses_in_matched_funding_states
-from public.fy2017_districts_deluxe_matr
-where
-include_in_universe_of_districts
-and district_type = 'Traditional'
-and postal_cd in
-(
+sum(case
+     when postal_cd in
+     (
 'MT',
 'ID',
 'MN',
@@ -27,3 +22,11 @@ and postal_cd in
 'FL',
 'ME'
 )
+
+
+         then current_known_unscalable_campuses + current_assumed_unscalable_campuses
+     else 0
+ end) as num_unscalable_campuses_with_state_match_fund
+from fy2017_districts_deluxe_matr
+where include_in_universe_of_districts
+and district_type = 'Traditional'
---
+++ Projects_SotS_2017/upgrades/upgraders_demog_2017.sql
@@ -0,0 +1,53 @@
+select
+(select count(*)
+from public.fy2017_districts_deluxe_matr
+where
+include_in_universe_of_districts
+and district_type = 'Traditional'
+and exclude_from_ia_analysis = 'False'
+) as "total_districts_in_universe_2017",
+
+(select count(*)
+from public.fy2017_districts_deluxe_matr
+where
+include_in_universe_of_districts
+and district_type = 'Traditional'
+and exclude_from_ia_analysis = 'False'
+and meeting_2014_goal_no_oversub) as "total_in_2017_meeting_2014",
+
+(select count(*)
+from public.fy2017_districts_deluxe_matr
+where
+include_in_universe_of_districts
+and district_type = 'Traditional'
+and exclude_from_ia_analysis = 'False'
+and meeting_2014_goal_no_oversub
+and meeting_2018_goal_oversub) as "total_in_2017_meeting_2018",
+
+
+(select count(*)
+from public.fy2017_districts_deluxe_matr
+where
+include_in_universe_of_districts
+and district_type = 'Traditional'
+and exclude_from_ia_analysis = 'False'
+and upgrade_indicator = 'True') as "total_upgraders_in_universe_2017",
+
+(select count(*)
+from public.fy2017_districts_deluxe_matr
+where
+include_in_universe_of_districts
+and district_type = 'Traditional'
+and exclude_from_ia_analysis = 'False'
+and upgrade_indicator = 'True'
+and meeting_2014_goal_no_oversub) as "total_upgraders_in_2017_meeting_2014",
+
+(select count(*)
+from public.fy2017_districts_deluxe_matr
+where
+include_in_universe_of_districts
+and district_type = 'Traditional'
+and exclude_from_ia_analysis = 'False'
+and upgrade_indicator = 'True'
+and meeting_2014_goal_no_oversub
+and meeting_2018_goal_oversub) as "total_upgraders_in_2017_meeting_2018"
