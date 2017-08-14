9a3427929b2408d11b5263d5285ffd73170213a5
Saaim <saaim.aslam@educationsuperhighway.org>
2017-08-14 09:35:37 -0700

    upgraders queries

--- Projects_SotS_2017/upgrades/net_new_upgraders.sql
+++ Projects_SotS_2017/upgrades/net_new_upgraders.sql
@@ -4,15 +4,28 @@ from public.fy2016_districts_deluxe_matr
 where
 include_in_universe_of_districts
 and district_type = 'Traditional'
-and upgrade_indicator = 'True'
-)
+and exclude_from_ia_analysis = 'False'
+and upgrade_indicator = 'True')
 
-select esh_id
-from
-public.fy2017_districts_deluxe_matr dd17
+select
+(select
+count(esh_id)
+from public.fy2017_districts_deluxe_matr
 where
-
 include_in_universe_of_districts
 and district_type = 'Traditional'
+and exclude_from_ia_analysis = 'False'
 and upgrade_indicator = 'True'
-and esh_id not in (select esh_id from a)
+and esh_id not in (select * from a)
+) as "net_new_upgrades_2017",
+
+(select
+count(esh_id)
+from public.fy2017_districts_deluxe_matr
+where
+include_in_universe_of_districts
+and district_type = 'Traditional'
+and exclude_from_ia_analysis = 'False'
+and upgrade_indicator = 'True'
+and esh_id in (select * from a)
+) as "repeat_upgrades_2017"
