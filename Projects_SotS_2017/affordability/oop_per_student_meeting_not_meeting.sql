bb2446de1645d315a2247d134bf00b90cbe40d3c
Justine <justine@educationsuperhighway.org>
2017-08-18 11:22:09 -0700

    costs and discounts

--- Projects_SotS_2017/affordability/oop_per_student_meeting_not_meeting.sql
+++ Projects_SotS_2017/affordability/oop_per_student_meeting_not_meeting.sql
@@ -1,7 +1,8 @@
 select 
   meeting_2014_goal_no_oversub::varchar,
   count(*) as num_districts,
-  median(ia_monthly_cost_total*(1-discount_rate_c1_matrix)/num_students) as oop_per_student
+  median(ia_monthly_cost_total*(1-discount_rate_c1_matrix)/num_students) as oop_per_student,
+  median(discount_rate_c1_matrix) as median_discount_rate
 from fy2017_districts_deluxe_matr dd17
 where dd17.include_in_universe_of_districts
 and dd17.district_type = 'Traditional'
@@ -13,8 +14,11 @@ UNION
 select 
   'overall' as meeting_2014_goal_no_oversub,
   count(*) as num_districts,
-  median(ia_monthly_cost_total*(1-discount_rate_c1_matrix)/num_students) as oop_per_student
+  median(ia_monthly_cost_total*(1-discount_rate_c1_matrix)/num_students) as oop_per_student,
+  median(discount_rate_c1_matrix) as median_discount_rate
 from fy2017_districts_deluxe_matr dd17
 where dd17.include_in_universe_of_districts
 and dd17.district_type = 'Traditional'
 and dd17.exclude_from_ia_cost_analysis = false
+
+--average discount rate
