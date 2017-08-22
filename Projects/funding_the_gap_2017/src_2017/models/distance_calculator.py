b942bdd9e1134b0e34f1d99d5ab801a3fc902dba
Justine <justine@educationsuperhighway.org>
2017-08-21 17:38:29 -0700

    distances partially calculated, waiting on steve to run in spiner

--- Projects/funding_the_gap_2017/src_2017/models/distance_calculator.py
+++ Projects/funding_the_gap_2017/src_2017/models/distance_calculator.py
@@ -32,19 +32,36 @@ campuses = campuses[(campuses.denomination == '1: Fit for FTG, Target')]
 campuses = campuses.reset_index(drop=True)
 
 ##calculate distance between all campuses and its district office and save into pandas dataframe
-campus_distances = []
-for i in range(0, campuses.shape[0]):
+#campus_distances = []
+for i in range(4718, campuses.shape[0]):
 	dist_test = distanceCalculator(	campuses['district_latitude'][i],
 									campuses['district_longitude'][i],
 									campuses['sample_campus_latitude'][i],
 									campuses['sample_campus_longitude'][i]).mapboxRequest()
 	campus_distances.append({'distance': dist_test})
-	time.sleep(1) 
+	time.sleep(2) # rate limit is 60 requests per minute
 print("Distances calculated")
 
+## for testing only
+import requests
+import sys
+sys.path.insert(0, GITHUB+'/Projects/funding_the_gap/src')
+from credentials import MAPBOX_ACCESS_TOKEN, COSTQUEST_USER_ID, COSTQUEST_PASS
+MAPBOX_URL = 'https://api.mapbox.com/directions/v5/mapbox/driving/'
+MAPBOX_URL_PARAMS = {'access_token': MAPBOX_ACCESS_TOKEN}
+r = requests.get("{0}{1},{2};{3},{4}.json".format(	MAPBOX_URL,
+													campuses['district_longitude'][i],
+													campuses['district_latitude'][i],
+													campuses['sample_campus_longitude'][i],
+													campuses['sample_campus_latitude'][i]), params = MAPBOX_URL_PARAMS)
+print(r.json())
+print(r.json()['code'])
+
+
+
 ##join distances to campuses and save
-campus_distances = DataFrame(campus_distances)
-campuses_distances = concat([campuses, campus_distances], axis=1)
+campus_distances_df = DataFrame(campus_distances)
+campuses_distances = concat([campuses, campus_distances_df], axis=1)
 
 campuses_distances.to_csv(GITHUB+'/Projects/funding_the_gap_2017/data/interim/campuses_distances.csv')
 print("File saved")
