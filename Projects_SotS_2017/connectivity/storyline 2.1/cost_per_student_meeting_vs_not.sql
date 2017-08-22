b779d87d25fc38ba8e317856413c669a3d35c4cc
Sierra Costanza <sierra@Sierras-MacBook-Pro.local>
2017-08-21 19:33:33 -0700

    modified cost per student after QA

--- Projects_SotS_2017/connectivity/storyline 2.1/cost_per_student_meeting_vs_not.sql
+++ Projects_SotS_2017/connectivity/storyline 2.1/cost_per_student_meeting_vs_not.sql
@@ -1,6 +1,7 @@
 with t as(
 SELECT esh_id, district_size, meeting_2014_goal_no_oversub,
-ia_monthly_cost_total, num_students
+(ia_monthly_cost_total*(1 - discount_rate_c1_matrix)::numeric)*12 as total_cost_district, 
+num_students
 from public.fy2017_districts_deluxe_matr del
 where  del.exclude_from_ia_analysis=false
 and include_in_universe_of_districts
@@ -10,15 +11,15 @@ and postal_cd !='AK')
 
 select 'all' as grp,
 meeting_2014_goal_no_oversub,
-median(ia_monthly_cost_total/num_students) as median_cost_per_student,
-sum(ia_monthly_cost_total)::numeric/sum(num_students) as avg_cost_per_student
+median(total_cost_district/num_students) as median_cost_per_student,
+sum(total_cost_district)::numeric/sum(num_students) as avg_cost_per_student
 from t
 group by 1,2
 union
 select 'mega/large' as grp,
 meeting_2014_goal_no_oversub,
-median(ia_monthly_cost_total/num_students) as median_cost_per_student,
-sum(ia_monthly_cost_total)::numeric/sum(num_students) as avg_cost_per_student
+median(total_cost_district/num_students) as median_cost_per_student,
+sum(total_cost_district)::numeric/sum(num_students) as avg_cost_per_student
 from t
 where district_size in ('Mega', 'Large')
 group by 1,2
