6653af9c9ca5e1171004293d77123d81be42889d
Saaim <saaim.aslam@educationsuperhighway.org>
2017-08-23 12:36:49 -0700

    updating to use orig_commitment_request instead of cmtd_commitment_request

--- Projects_SotS_2017/e-rate/2014_funding_commitments.sql
+++ Projects_SotS_2017/e-rate/2014_funding_commitments.sql
@@ -1,6 +1,6 @@
 select
 (
-select sum(cmtd_commitment_request)::numeric/1000000000 as funding_committment_request_billions
+select sum(orig_commitment_request)::numeric/1000000000 as funding_committment_request_billions
 
 from
 public.funding_requests
@@ -8,8 +8,9 @@ where
 funding_year = '2014'
 and cmtd_category_of_service = 'TELCOMM SERVICES') as "funding_for_voice_2014",
 
+-- voice funding might not possible, Telomm services does not represent voice - keeping it here for now for discusion
 (
-select sum(cmtd_commitment_request)::numeric/1000000000 as funding_committment_request_billions
+select sum(orig_commitment_request)::numeric/1000000000 as funding_committment_request_billions
 from
 public.funding_requests
 where
