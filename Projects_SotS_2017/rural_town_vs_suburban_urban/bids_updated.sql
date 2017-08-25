a96c7d9cb5a383202e5871c3b67f29ae628b15ac
Sierra Costanza <sierra@Sierras-MacBook-Pro.local>
2017-08-25 13:05:55 -0700

    updates to rural bids logic

---
+++ Projects_SotS_2017/rural_town_vs_suburban_urban/bids_updated.sql
@@ -0,0 +1,38 @@
+select t.locale, 
+sum(1) as num_districts,
+sum(case when district_received_0_bids = 1 then 1 else 0 end) as num_received_0_bids,
+median(num_bids_received) as median_bids_received,
+avg(num_bids_received) avg_bids_received
+from (
+select dd.esh_id,
+    case when locale in ('Rural','Town') then 'Rural/Town' else 'Suburban/Urban' end as locale,
+    sum(frns.num_bids_received::numeric) as num_bids_received,
+    sum(case
+          when frns.num_bids_received::numeric = 0 or frns.num_bids_received is null
+            then 1
+          else 0
+        end) as num_frns_with_0_bids,
+    case
+      when sum(frns.num_bids_received::numeric) = 0
+        or sum(frns.num_bids_received::numeric) is null
+        then 1
+      else 0
+    end as district_received_0_bids
+    from fy2017.frns frns
+    
+    left join public.entity_bens eb
+    on frns.ben = eb.ben
+    
+    left join public.fy2017_district_lookup_matr dl
+    on dl.district_esh_id = eb.entity_id::varchar
+    
+    join public.fy2017_districts_deluxe_matr dd
+    on dl.district_esh_id = dd.esh_id
+    
+    where 
+    include_in_universe_of_districts
+    and district_type = 'Traditional'
+    
+    group by 1,2)
+  t
+  group by 1
---
+++ Projects_SotS_2017/rural_town_vs_suburban_urban/cost comparison.sql
@@ -0,0 +1,1 @@
+case when locale in ('Rural','Town') then 'Rural/Town' else 'Suburban/Urban' end as locale,
