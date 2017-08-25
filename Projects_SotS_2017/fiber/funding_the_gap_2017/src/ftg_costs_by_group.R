ad551ae9ef36d4cda0d783d7ab024c3dec1c7f03
Justine <justine@educationsuperhighway.org>
2017-08-25 16:19:01 -0700

    split by DR and locale

---
+++ Projects_SotS_2017/fiber/funding_the_gap_2017/src/ftg_costs_by_group.R
@@ -0,0 +1,58 @@
+## =========================================
+##
+## % of total by locale and discount pct
+##
+## =========================================
+
+## Clearing memory
+rm(list=ls())
+
+## set working directory
+setwd("C:/Users/jesch/OneDrive/Documents/GitHub/ficher/")
+
+## load packages (if not already in the environment)
+packages.to.install <- c("dplyr", "ggplot2", "reshape2")
+for (i in 1:length(packages.to.install)){
+  if (!packages.to.install[i] %in% rownames(installed.packages())){
+    install.packages(packages.to.install[i])
+  }
+}
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+
+##import
+wan_costs <- read.csv("Projects_SotS_2017/fiber/funding_the_gap_2017/data/interim/campus_build_costs.csv")
+ia_costs <- read.csv("Projects/funding_the_gap_2017/data/interim/district_build_costs.csv")
+
+wan_costs <- select(wan_costs, district_locale, c1_discount_rate_or_state_avg, build_fraction_wan, total_cost_median)
+ia_costs <- select(ia_costs, district_locale, c1_discount_rate_or_state_avg, build_fraction_ia, total_cost_ia)
+
+names(wan_costs) <- c("locale", "discount", "builds", "cost")
+names(ia_costs) <- c("locale", "discount", "builds", "cost")
+
+costs <- rbind(wan_costs, ia_costs)
+
+costs <- 
+  costs %>% 
+  mutate(locale = case_when(locale == 'Town' ~ "Rural",
+                            locale == 'Suburban' ~ "Urban",
+                            TRUE ~ as.character(locale)))
+costs <- 
+  costs %>% 
+  mutate(discount = case_when(discount >= .8 ~ "free",
+                              discount < .8 ~ "half free"))
+
+locale_discount <- 
+  costs %>% 
+  group_by(locale, discount) %>% 
+  summarise(pct_cost = sum(cost)/sum(costs$cost),
+            avg_cost = sum(cost)/sum(builds))
+
+locale <- 
+  costs %>% 
+  group_by(locale) %>% 
+  summarise(pct_cost = sum(cost)/sum(costs$cost),
+            avg_cost = sum(cost)/sum(builds))
+
+
