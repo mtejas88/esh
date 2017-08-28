a69da7d24e9770b6843662ba440e7c0f47683372
jamie-barnes <jamie.barnes@educationsuperhighway.org>
2017-08-28 13:15:34 -0700

    updating logic to include excluded line items
    updating logic to include excluded line items in 2017 and 2016 due to DQ
    discrepencies we discussed. also expanded it to count FRNs applied for
    by districts or schools within our population (and not just received)
    because i noticed state match FRNs going to NIFs that I think should be
    included. both of these will make our numbers more "USACy" which I think
    is better for analysis like this.
    
    FYI to fellow Js @justineschott @jeremyholtzman and can one of you QA my
    updates?

--- Projects_SotS_2017/fiber/state_match/2016_2017_state_match.sql
+++ Projects_SotS_2017/fiber/state_match/2016_2017_state_match.sql
@@ -11,21 +11,35 @@ with a as (
 		inner join public.fy2017_esh_line_items_v eli
 		on eli.frn = f.frn
 
-		inner join public.fy2017_services_received_matr sr
+		left join public.fy2017_services_received_matr sr
 		on eli.id = sr.line_item_id
 
-		inner join public.fy2017_districts_deluxe_matr dd
+		left join public.fy2017_districts_deluxe_matr dd
 		on dd.esh_id::numeric = sr.applicant_id
 
 		where match_amount::numeric > 0
 
-		/* frn includes at least one line item that is not excluded*/
-		and sr.inclusion_status != 'dqs_excluded'
-		/* frn includes at least one line item that is received by districts in our population*/
-		and dd.include_in_universe_of_districts = true
-		and dd.district_type = 'Traditional'
-
 		and f.frn not in (select frn from fy2017.current_frns)
+
+		and (
+		/* frn either includes at least one line item that is received by districts in our population*/
+		 (dd.include_in_universe_of_districts = true and dd.district_type = 'Traditional')
+
+		 or 
+
+		/* frn filed by school or district in our population */ 
+		 eli.applicant_ben in (select eb.ben
+							from public.fy2017_district_lookup_matr dl
+
+							inner join public.entity_bens eb
+							on eb.entity_id = dl.esh_id::numeric
+
+							inner join public.fy2017_districts_deluxe_matr dd
+							on dl.district_esh_id = dd.esh_id
+
+							where dd.include_in_universe_of_districts = true
+							and dd.district_type = 'Traditional')
+		 	)
 
 		group by f.frn,
 		f.match_amount,
@@ -45,19 +59,33 @@ with a as (
 		inner join public.fy2017_esh_line_items_v eli
 		on eli.frn = f.frn
 
-		inner join public.fy2017_services_received_matr sr
+		left join public.fy2017_services_received_matr sr
 		on eli.id = sr.line_item_id
 
-		inner join public.fy2017_districts_deluxe_matr dd
+		left join public.fy2017_districts_deluxe_matr dd
 		on dd.esh_id::numeric = sr.applicant_id
 
 		where match_amount::numeric > 0
 
-		/* frn includes at least one line item that is not excluded*/
-		and sr.inclusion_status != 'dqs_excluded'
-		/* frn includes at least one line item that is received by districts in our population*/
-		and dd.include_in_universe_of_districts = true
-		and dd.district_type = 'Traditional'
+		and (
+		/* frn either includes at least one line item that is received by districts in our population*/
+		 (dd.include_in_universe_of_districts = true and dd.district_type = 'Traditional')
+
+		 or 
+
+		/* frn filed by school or district in our population */ 
+		 eli.applicant_ben in (select eb.ben
+							from public.fy2017_district_lookup_matr dl
+
+							inner join public.entity_bens eb
+							on eb.entity_id = dl.esh_id::numeric
+
+							inner join public.fy2017_districts_deluxe_matr dd
+							on dl.district_esh_id = dd.esh_id
+
+							where dd.include_in_universe_of_districts = true
+							and dd.district_type = 'Traditional')
+		 	)
 
 		group by f.frn,
 		f.match_amount,
@@ -76,21 +104,35 @@ with a as (
 		inner join fy2016.line_items eli
 		on eli.frn = f.frn
 
-		inner join public.fy2016_services_received_matr sr
+		left join public.fy2016_services_received_matr sr
 		on eli.id = sr.line_item_id
 
-		inner join public.fy2016_districts_deluxe_matr dd
+		left join public.fy2016_districts_deluxe_matr dd
 		on dd.esh_id::numeric = sr.applicant_id
 
 		where match_amount::numeric > 0
 
-		/* frn includes at least one line item that is not excluded*/
-		and sr.inclusion_status != 'dqs_excluded'
-		/* frn includes at least one line item that is received by districts in our population*/
-		and dd.include_in_universe_of_districts = true
-		and dd.district_type = 'Traditional'
-
 		and f.frn not in (select frn from fy2016.current_frns)
+
+		and (
+		/* frn either includes at least one line item that is received by districts in our population*/
+		 (dd.include_in_universe_of_districts = true and dd.district_type = 'Traditional')
+
+		 or 
+
+		/* frn filed by school or district in our population */ 
+		 eli.applicant_ben in (select eb.ben
+							from public.fy2016_district_lookup_matr dl
+
+							inner join public.entity_bens eb
+							on eb.entity_id = dl.esh_id::numeric
+
+							inner join public.fy2016_districts_deluxe_matr dd
+							on dl.district_esh_id = dd.esh_id
+
+							where dd.include_in_universe_of_districts = true
+							and dd.district_type = 'Traditional')
+		 	)
 		
 		group by f.frn,
 		f.match_amount,
@@ -98,7 +140,7 @@ with a as (
 
 	union 
 
-		/* 2017 FRNs that have been updated/are in current view and are approved*/
+		/* 2016 FRNs that have been updated/are in current view and are approved*/
 		select 2016 as year,
 		f.frn_status,
 		f.frn,
@@ -109,25 +151,38 @@ with a as (
 		inner join fy2016.line_items eli
 		on eli.frn = f.frn
 
-		inner join public.fy2016_services_received_matr sr
+		left join public.fy2016_services_received_matr sr
 		on eli.id = sr.line_item_id
 
-		inner join public.fy2016_districts_deluxe_matr dd
+		left join public.fy2016_districts_deluxe_matr dd
 		on dd.esh_id::numeric = sr.applicant_id
 
 		where match_amount::numeric > 0
 
-		/* frn includes at least one line item that is not excluded*/
-		and sr.inclusion_status != 'dqs_excluded'
-		/* frn includes at least one line item that is received by districts in our population*/
-		and dd.include_in_universe_of_districts = true
-		and dd.district_type = 'Traditional'
+		and (
+		/* frn either includes at least one line item that is received by districts in our population*/
+		 (dd.include_in_universe_of_districts = true and dd.district_type = 'Traditional')
+
+		 or 
+
+		/* frn filed by school or district in our population */ 
+		 eli.applicant_ben in (select eb.ben
+							from public.fy2016_district_lookup_matr dl
+
+							inner join public.entity_bens eb
+							on eb.entity_id = dl.esh_id::numeric
+
+							inner join public.fy2016_districts_deluxe_matr dd
+							on dl.district_esh_id = dd.esh_id
+
+							where dd.include_in_universe_of_districts = true
+							and dd.district_type = 'Traditional')
+		 	)
 		
 
 		group by f.frn,
 		f.match_amount,
 		f.frn_status)
-
 
 select year,
 frn_status,
