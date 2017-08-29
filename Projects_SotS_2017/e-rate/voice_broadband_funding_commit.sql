cd7a545f4671e4394acbaad8e4f64a315a3710a8
Saaim <saaim.aslam@educationsuperhighway.org>
2017-08-28 19:42:00 -0700

    updating with include in universe filtering logic and removing dups

--- Projects_SotS_2017/e-rate/voice_broadband_funding_commit.sql
+++ Projects_SotS_2017/e-rate/voice_broadband_funding_commit.sql
@@ -6,8 +6,21 @@ select  2015 as funding_year,
             then 'c2'
           else 'broadband'
         end as category,
-        sum(round(frns."Total Elig Chg"::numeric * (frns."Discount"::numeric/100),2))/1000000000 as funding_commitment_request_$B
+        sum(distinct(round(frns."Total Elig Chg"::numeric * (frns."Discount"::numeric/100),2)))/1000000000 as funding_commitment_request_$B
 from public.fy2015_funding_request_key_informations frns
+
+left join public.fy2015_basic_information_and_certifications bi
+on frns."Application Number" = bi."Application Number"
+
+left join public.entity_bens eb
+on bi."BEN" = eb.ben
+
+left join public.district_lookup_2015_m dl
+on dl.district_esh_id = eb.entity_id
+
+join public.fy2015_districts_deluxe_m dd
+on dl.district_esh_id = dd.esh_id
+
 group by 1, 2
 
 UNION
@@ -22,8 +35,24 @@ select  2016 as funding_year,
             then 'c2'
           else 'broadband'
         end as category,
-        sum(frns.funding_commitment_request::numeric)/1000000000 as funding_commitment_request_$B
-from fy2016.frns
+        sum(distinct frns.funding_commitment_request::numeric)/1000000000 as funding_commitment_request_$B
+from fy2016.frns frns
+
+left join fy2016.basic_informations bi
+on frns.application_number = bi.application_number
+
+left join public.entity_bens eb
+on bi.billed_entity_number = eb.ben
+
+left join public.fy2016_district_lookup_matr dl
+on dl.district_esh_id = eb.entity_id::varchar
+
+join public.fy2016_districts_deluxe_matr dd
+ on dl.district_esh_id = dd.esh_id
+
+where
+include_in_universe_of_districts
+and district_type = 'Traditional'
 group by 1, 2
 
 UNION
@@ -38,7 +67,19 @@ select  2017 as funding_year,
             then 'c2'
           else 'broadband'
         end as category,
-        sum(funding_commitment_request::numeric)/1000000000 as funding_commitment_request_$B
+        sum(distinct funding_commitment_request::numeric)/1000000000 as funding_commitment_request_$B
 from fy2017.frns
+left join public.entity_bens eb
+on frns.ben = eb.ben
+
+left join public.fy2017_district_lookup_matr dl
+on dl.district_esh_id = eb.entity_id::varchar
+
+join public.fy2017_districts_deluxe_matr dd
+ on dl.district_esh_id = dd.esh_id
+
+where
+include_in_universe_of_districts
+and district_type = 'Traditional'
 group by 1, 2
 order by funding_year, category
