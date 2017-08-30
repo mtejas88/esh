0136a753d172648726b0de789f52efe208f05c9d
Saaim <saaim.aslam@educationsuperhighway.org>
2017-08-30 12:42:41 -0700

    new versions of unscalables

---
+++ Projects_SotS_2017/connectivity/unscalables_state_match/unscal_all_state_match.sql
@@ -0,0 +1,9 @@
+select postal_cd, sum(current_assumed_unscalable_campuses + current_known_unscalable_campuses) as unscalable_campuses_all
+from public.fy2017_districts_deluxe_matr dd
+where dd.include_in_universe_of_districts
+and dd.district_type = 'Traditional'
+and fiber_target_status = 'Target'
+and postal_cd in
+
+('ID','AZ','VA','NC','CA','NM','NY','OK','FL','ME','MA','MT','MO','TX','NV','MD','NH','CO','MN','KS','IL')
+group by 1
