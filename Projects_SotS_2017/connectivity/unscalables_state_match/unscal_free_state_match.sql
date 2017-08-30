82c5f2b93a0fc83900799f430361fad2f5988fb0
Saaim <saaim.aslam@educationsuperhighway.org>
2017-08-30 10:24:37 -0700

    removing the fiber target filtering

--- Projects_SotS_2017/connectivity/unscalables_state_match/unscal_free_state_match.sql
+++ Projects_SotS_2017/connectivity/unscalables_state_match/unscal_free_state_match.sql
@@ -2,7 +2,6 @@ select postal_cd, sum(current_assumed_unscalable_campuses + current_known_unscal
 from public.fy2017_districts_deluxe_matr dd
 where dd.include_in_universe_of_districts
 and dd.district_type = 'Traditional'
-and fiber_target_status = 'Target'
 and discount_rate_c1_matrix >= '0.8'
 and postal_cd in
 
