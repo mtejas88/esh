f6292338fa46de851de2c9c3e24a534bc0b0c705
jeremyholtzman <jeremyholtzman@gmail.com>
2017-08-29 17:29:11 -0700

    removed erate condition from perc IA. calc current wan cost

--- Projects_SotS_2017/e-rate/AK_increase/src/total_wan_costs.sql
+++ Projects_SotS_2017/e-rate/AK_increase/src/total_wan_costs.sql
@@ -1,7 +1,3 @@
-/*STEPS
-0. Find % clean SR $ that are IA in AK
-1. Find % clean SR $ that are IA not in AK
-*/
 with li_lookup as (
   select (dd.postal_cd = 'AK') as postal_cd_AK,
   line_item_id, 
@@ -33,7 +29,9 @@ with li_lookup as (
           or 'exclude_for_cost_only_unknown' = any(sr.open_tags))
   )
 
-)
+),
+
+perc as (
 
   select distinct
     postal_cd_AK,
@@ -45,21 +43,44 @@ with li_lookup as (
 
   from li_lookup
 
+),
 
-/* Methodology 
-0. Find % clean SR $ that are IA in AK
-1. Find % clean SR $ that are IA not in AK
-2. Find total $ in SR
-3. Find total $ in SR in AK
-4. Find extrap $ IA in AK (0 * 3)
-5. Find total $ in SR not in AK (2 - 3)
-6. Find extrap $ IA not in AK (5 * 1)
-7. Find wtd avg bw/student in AK in 0
-8. Find wtd avg bw/student not in AK in 1
-9. Find 1 Mbps cost, bw/student not in AK
-10. Find 1 Mbps bw/student in AK
-11. Find change in cost, change in BW not in AK (6,8,9)
-12. Find change in BW in AK (7, 10)
-13. Find 1 Mbps cost in AK (4, 11, 12)
-14. Find WAN for everyone
-*/
+lines as (
+
+  select (dd.postal_cd = 'AK') as postal_cd_AK,
+    line_item_id,
+    recipient_id,
+    line_item_district_mrc_unless_null
+
+  from public.fy2017_services_received_matr sr
+
+  join public.fy2017_districts_deluxe_matr dd
+  on sr.recipient_id = dd.esh_id
+
+  where inclusion_status != 'dqs_excluded'
+  and dd.include_in_universe_of_districts = true
+  and dd.district_type = 'Traditional'
+
+),
+
+costs as (
+ 
+select postal_cd_AK,
+round(sum(line_item_district_mrc_unless_null * 12),0) as total_costs
+
+from lines
+group by 1
+
+)
+
+select 
+  perc.postal_cd_ak,
+  purpose_adj,
+  round(perc_monthly_cost * total_costs,0) as extrap_wan_cost
+
+from perc
+
+join costs
+on perc.postal_cd_AK = costs.postal_cd_AK
+
+where purpose_adj = 'WAN'
