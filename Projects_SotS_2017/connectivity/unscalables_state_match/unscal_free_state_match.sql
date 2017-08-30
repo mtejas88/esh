edbc3de4b1be5f5f0a2fd06359944620d2fa34e8
Saaim <saaim.aslam@educationsuperhighway.org>
2017-08-30 10:24:37 -0700

    removing the fiber target filtering

---
+++ Projects_SotS_2017/connectivity/unscalables_state_match/unscal_free_state_match.sql
@@ -0,0 +1,9 @@
+select postal_cd, sum(current_assumed_unscalable_campuses + current_known_unscalable_campuses) as unscalable_campuses_free
+from public.fy2017_districts_deluxe_matr dd
+where dd.include_in_universe_of_districts
+and dd.district_type = 'Traditional'
+and discount_rate_c1_matrix >= '0.8'
+and postal_cd in
+
+('ID','AZ','VA','NC','CA','NM','NY','OK','FL','ME','MA','MT','MO','TX','NV','MD','NH','CO','MN','KS','IL')
+group by 1
