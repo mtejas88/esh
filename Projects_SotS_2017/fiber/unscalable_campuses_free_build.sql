c39c9e00d7f2af7faf14aa8f1b6019c8595042b4
Justine <justine@educationsuperhighway.org>
2017-08-30 12:01:32 -0700

    real updates

--- Projects_SotS_2017/fiber/unscalable_campuses_free_build.sql
+++ Projects_SotS_2017/fiber/unscalable_campuses_free_build.sql
@@ -18,17 +18,6 @@ sum( case
                 end)::numeric as pct_of_state_match_w_high_dr,
 sum( case
         when postal_cd in ( 'AZ', 'CA', 'CO', 'FL', 'ID', 'MA', 'MD', 'ME', 'MO', 'MT', 'NC', 'NH', 'NM', 'NV', 'NY', 'OK', 'TX', 'VA')
-        and discount_rate_c1_matrix >= .8
-          then current_assumed_unscalable_campuses+current_known_unscalable_campuses
-        else 0
-      end)/sum( case
-                  when postal_cd in ( 'AZ', 'CA', 'CO', 'FL', 'ID', 'MA', 'MD', 'ME', 'MO', 'MT', 'NC', 'NH', 'NM', 'NV', 'NY', 'OK', 'TX', 'VA')
-                  and discount_rate_c1_matrix is not null
-                    then current_assumed_unscalable_campuses+current_known_unscalable_campuses
-                  else 0
-                end)::numeric as pct_of_state_match_w_high_dr,
-sum( case
-        when postal_cd in ( 'AZ', 'CA', 'CO', 'FL', 'ID', 'MA', 'MD', 'ME', 'MO', 'MT', 'NC', 'NH', 'NM', 'NV', 'NY', 'OK', 'TX', 'VA')
           then current_assumed_unscalable_campuses+current_known_unscalable_campuses
         else 0
       end)/sum(current_assumed_unscalable_campuses+current_known_unscalable_campuses)::numeric as pct_state_match
