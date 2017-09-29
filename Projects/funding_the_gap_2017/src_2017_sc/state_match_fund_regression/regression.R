b4221c697970254ebe82072b72b092270fa60d97
Sierra Costanza <sierra@Sierras-MacBook-Pro.local>
2017-09-29 11:10:14 -0700

    final (initial) regression

--- Projects/funding_the_gap_2017/src_2017_sc/state_match_fund_regression/regression.R
+++ Projects/funding_the_gap_2017/src_2017_sc/state_match_fund_regression/regression.R
@@ -90,12 +90,12 @@ w=1/(reg$fitted.values^2)
 regw.2 = lm(y_midrange ~ ., weights=w, data = state.metrics.2016.14.regression)
 summary(regw.2) #better in terms of R^2 and coefficient SEs
 
-  ## Iterative, to improve
-  fit.w = reg
-  for (i in 1:100) {
-    w = 1/(fit.w$fitted.values^2)
-    fit.w = lm(y_midrange ~ ., weights=w, data = state.metrics.2016.14.regression)
-  } #confirmed with more iterations that this has converged
+## Iterative, to improve
+fit.w = reg
+for (i in 1:100) {
+  w = 1/(fit.w$fitted.values^2)
+  fit.w = lm(y_midrange ~ ., weights=w, data = state.metrics.2016.14.regression)
+} #confirmed with more iterations that this has converged
 
 summary(fit.w)
 
@@ -126,8 +126,8 @@ state.metrics.2017 <- state.metrics.2017[state.metrics.2017$min_total_state_fund
 
 state.metrics.2017$ratio_miles_per_build = state.metrics.2017$max_miles_per_build/state.metrics.2017$min_miles_per_build
 state.metrics.2017$ratio_total_cost_wan = ifelse((state.metrics.2017$total_cost_az_pop_wan - state.metrics.2017$min_total_cost_wan) > 0,
-                                                    (state.metrics.2017$total_cost_az_wan - state.metrics.2017$min_total_cost_wan)/(state.metrics.2017$total_cost_az_pop_wan - state.metrics.2017$min_total_cost_wan),
-                                                    (state.metrics.2017$total_cost_az_pop_wan - state.metrics.2017$min_total_cost_wan)/(state.metrics.2017$total_cost_az_wan - state.metrics.2017$min_total_cost_wan))
+                                                 (state.metrics.2017$total_cost_az_wan - state.metrics.2017$min_total_cost_wan)/(state.metrics.2017$total_cost_az_pop_wan - state.metrics.2017$min_total_cost_wan),
+                                                 (state.metrics.2017$total_cost_az_pop_wan - state.metrics.2017$min_total_cost_wan)/(state.metrics.2017$total_cost_az_wan - state.metrics.2017$min_total_cost_wan))
 
 state.metrics.2017.regression = state.metrics.2017[, names(state.metrics.2017) %in% c('ratio_miles_per_build','ratio_total_cost_wan','min_total_state_funding','max_total_state_funding')]
 
@@ -144,6 +144,29 @@ dim(result_export[result_export$prediction_wf < 0,])
 
 result_export = result_export %>% mutate(prediction=ifelse(prediction_wm < min_2017 , min_2017, prediction_wm))
 result_export = result_export[, names(result_export) %in% c("postal_cd", "min_2017", "max_2017", "prediction")]
+result_export = result_export %>% mutate(prediction_lwr = 
+                                           ifelse(result_export$prediction == result_export$min_2017,result_export$min_2017,
+                                                  result_export$prediction - (.2*result_export$prediction)),
+                                         prediction_upr = 
+                                           ifelse(result_export$prediction >= result_export$max_2017,result_export$max_2017,
+                                                  result_export$prediction + (.2*result_export$prediction)))
+result_export$approved_state = ifelse(result_export$postal_cd %in% c('AZ',
+                                                                     'CO',
+                                                                     'FL',
+                                                                     'ID',
+                                                                     'MA',
+                                                                     'MD',
+                                                                     'ME',
+                                                                     'MO',
+                                                                     'MT',
+                                                                     'NC',
+                                                                     'NH',
+                                                                     'NM',
+                                                                     'NV',
+                                                                     'NY',
+                                                                     'OK',
+                                                                     'TX',
+                                                                     'VA'),"Already approved/known","To estimate")
 
 write.csv(result_export, file = "initial_results_2017.csv")
 
