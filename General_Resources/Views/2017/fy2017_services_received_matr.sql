d3f7c27f1519f21c8fcf5ffa17a162056e1c1ce4
jamie-barnes <jamie.barnes@educationsuperhighway.org>
2017-10-09 14:20:10 -0700

    hard coded school ids, removing charter tag, leaving excluded flag

--- General_Resources/Views/2017/fy2017_services_received_matr.sql
+++ General_Resources/Views/2017/fy2017_services_received_matr.sql
@@ -64,8 +64,7 @@ FROM (
             CASE
               WHEN  'exclude' = any(li.open_flag_labels) or
                     'canceled' = any(li.open_flag_labels) or
-                    'video_conferencing' = any(li.open_flag_labels) or
-                    'charter_service' = any(li.open_tag_labels) 
+                    'video_conferencing' = any(li.open_flag_labels)
                 THEN 'dqs_excluded'
               WHEN ('exclude_for_cost_only_free' = any(li.open_tag_labels) 
                 OR 'exclude_for_cost_only_restricted' = any(li.open_tag_labels) 
