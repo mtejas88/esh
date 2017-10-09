c462a09a3bba38c3fa5e50bb77dd11b8a2460f51
jamie-barnes <jamie.barnes@educationsuperhighway.org>
2017-10-09 09:49:11 -0700

    reorg project

--- Projects/charter_research/src/charter_classification/main_query.sql
+++ Projects/charter_research/src/charter_classification/main_query.sql
@@ -1,8 +1,8 @@
 
-with entity_demog as (
+with entity_demog_big as (
 -- classifies all entities using hybrid of entity table, dd, and SFDC 
 -- note this will create duplicate records for entities that have more than one ben mapped to them
-	select distinct e.entity_id as entity_esh_id,
+	select e.entity_id as entity_esh_id,
 	eb.ben as entity_ben,
 	case
 		-- districts
@@ -91,8 +91,27 @@ with entity_demog as (
 	on p_dd.esh_id = a.esh_id__c
 ),
 
+entity_demog as (select entity_esh_id,
+	entity_ben,
+	entity_class,
+	entity_campus_id,
+	parent_district_esh_id,
+	parent_district_class
+
+	from entity_demog_big
+
+	group by 
+	entity_esh_id,
+	entity_ben,
+	entity_class,
+	entity_campus_id,
+	parent_district_esh_id,
+	parent_district_class
+),
+
+
 -- limit to campuses in regular districts that contain regular school
-rs_campus as (select distinct entity_campus_id,
+rs_campus as (select entity_campus_id,
 	parent_district_esh_id
 
 	from entity_demog 
@@ -124,7 +143,7 @@ cs_1 as (select
 -- applicant to recipient relationship
 -- note: intentionally using the least conservative method to define these relationships.
 -- e.g. not limiting to broadband, not using allocations, not removing anything excluded or denied, adding current 2017
-serv as (select u.applicant_ben,
+serv_big as (select u.applicant_ben,
 		--applicant info
 		ed_a.entity_esh_id as applicant_entity_id,
 		ed_a.entity_class as applicant_entity_class,
@@ -144,7 +163,8 @@ serv as (select u.applicant_ben,
 
 		left join entity_demog ed_r
 		on u.ben = ed_r.entity_ben
-
+--commenting out because i think this might take too long
+/*
 	union 
 		select u.applicant_ben,
 		--applicant info
@@ -165,20 +185,48 @@ serv as (select u.applicant_ben,
 		on u.applicant_ben = ed_a.entity_ben 
 
 		left join entity_demog ed_r
-		on u.ben = ed_r.entity_ben
+		on u.ben = ed_r.entity_ben */
+),
+
+serv as (select 
+	applicant_entity_id,
+	applicant_entity_class,
+	applicant_district_entity_id,
+	applicant_district_entity_class,
+	recipient_entity_id,
+	recipient_entity_class,
+	recipient_district_entity_id,
+	recipient_district_entity_class
+
+	from serv_big
+
+	group by applicant_entity_id,
+	applicant_entity_class,
+	applicant_district_entity_id,
+	applicant_district_entity_class,
+	recipient_entity_id,
+	recipient_entity_class,
+	recipient_district_entity_id,
+	recipient_district_entity_class
 ),
 
 -- limit to applicants that serve regular schools other than their districts
-reg_serv as (select *
+reg_serv as (select applicant_entity_id,
+	recipient_district_entity_id
+
 	from serv
 	where recipient_entity_class = 'School'
 	and recipient_district_entity_class = 'Traditional District'
 	and applicant_entity_id != recipient_district_entity_id
+
+	group by applicant_entity_id,
+	recipient_district_entity_id
 ),
 
 
-charter_recip as (select cs_1.charter_esh_id,
+charter_recip_big as (select cs_1.charter_esh_id,
 	cs_1.district_esh_id,
+	cs_1.shared_campus_reg,
 	--file for themselves
 	case
 		when serv.applicant_entity_id = charter_esh_id
@@ -224,12 +272,39 @@ charter_recip as (select cs_1.charter_esh_id,
 	on reg_serv.recipient_district_entity_id = cs_1.district_esh_id
 ),
 
+charter_recip as (select charter_esh_id,
+	district_esh_id,
+	shared_campus_reg,
+	applicant_self,
+	applicant_own_district,
+	applicant_servs_reg_schools,
+	applicant_other_charter_school,
+	applicant_charter_district,
+	applicant_consortia,
+	applicant_consoria_not_reg,
+	all_applicants
+
+	from charter_recip_big
+
+	group by charter_esh_id,
+	district_esh_id,
+	shared_campus_reg,
+	applicant_self,
+	applicant_own_district,
+	applicant_servs_reg_schools,
+	applicant_other_charter_school,
+	applicant_charter_district,
+	applicant_consortia,
+	applicant_consoria_not_reg,
+	all_applicants
+),
 charter_recip_agg as (select charter_esh_id,
 	district_esh_id,
+	shared_campus_reg,
 	count(distinct applicant_self) as applicant_self,
 	count(distinct applicant_own_district) as applicant_own_district,
 	count(distinct applicant_servs_reg_schools) as applicant_servs_reg_schools,
-	count(distinct applicant_other_charter_school) as applicant_other_charter_school,
+	count(distinct applicant_other_charter_school) as applicant_other_charter_school
 	count(distinct applicant_charter_district) as applicant_charter_district,
 	count(distinct applicant_consortia) as applicant_consortia,
 	count(distinct applicant_consoria_not_reg) as applicant_consoria_not_reg,
@@ -237,9 +312,21 @@ charter_recip_agg as (select charter_esh_id,
 
 	from charter_recip
 
+
 	group by charter_esh_id,
-	district_esh_id)
+	district_esh_id,
+	shared_campus_reg
+)
 
-select *
+select charter_esh_id,
+district_esh_id
 
-from charter_recip_agg
+from charter_recip_agg 
+
+where shared_campus_reg = true
+or (applicant_own_district > 0
+	and applicant_self = 0
+	and applicant_charter_district = 0
+	and applicant_other_charter_school = 0)
+
+	
