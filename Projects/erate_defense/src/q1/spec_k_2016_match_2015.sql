48c9399ce7070968a066ae6505bfd45ac1b9dc83
Sierra Costanza <sierra@Sierras-MacBook-Pro.local>
2017-10-09 14:39:37 -0700

    starting q1

---
+++ Projects/erate_defense/src/q1/spec_k_2016_match_2015.sql
@@ -0,0 +1,292 @@
+with bi_2016 as (
+  select 
+    application_number,
+    category_of_service,
+    null as form471_url
+  
+  from fy2016.current_basic_informations 
+  UNION
+  select
+    application_number,
+    category_of_service,
+    form471_url
+  from fy2016.basic_informations 
+  where application_number not in (
+    select distinct application_number
+    from fy2016.current_basic_informations
+  )
+),
+
+frns_16 as (
+
+  select frn.application_number,
+  frn.frn,
+  frn.frn_status,
+  frn.fiber_sub_type,
+  fli.line_item,
+  fli.function,
+  fli.type_of_product,
+  fli.total_monthly_eligible_recurring_costs,
+  fli.total_eligible_one_time_costs
+  
+  from fy2016.frns frn
+  
+  left join fy2016.frn_line_items fli
+  on frn.frn = fli.frn
+  
+  where frn.frn not in (
+    select frn
+    from fy2016.current_frns
+  )
+  
+  union
+  
+  select frn.application_number,
+  frn.frn,
+  frn.frn_status,
+  frn.fiber_sub_type,
+  fli.line_item,
+  fli.function,
+  fli.type_of_product,
+  fli.total_monthly_eligible_recurring_costs,
+  fli.total_eligible_one_time_costs
+  
+  from fy2016.current_frns frn
+  
+  left join fy2016.current_frn_line_items fli
+  on frn.frn = fli.frn
+
+),
+
+--final denied FRNs, to match to 2015 line items
+final_2016 as (
+
+select distinct
+  fr.application_number,
+  fr.frn,
+  fr.frn_status,
+  fr.review_status,
+  fr.ben,
+  fr.funding_year,
+  fr.fcdl_comment_for_frn,
+  fr.fcdl_comment_for_application,
+  fr.application_status,
+  bi16.category_of_service as category_of_service,
+  frns_16.fiber_sub_type as fiber_sub_type,
+  frns_16.total_monthly_eligible_recurring_costs,
+  frns_16.total_eligible_one_time_costs,
+  case when frns_16.function = 'Fiber' and type_of_product not in ('Dark Fiber IRU (No Special Construction)',
+                                                       'Dark Fiber (No Special Construction)')
+        then 'Lit Fiber'
+        
+        when frns_16.function = 'Fiber' AND type_of_product in ('Dark Fiber IRU (No Special Construction)',
+                                                    'Dark Fiber (No Special Construction)')
+          OR
+            frns_16.function = 'Fiber Maintenance & Operations'
+         then 'Dark Fiber'
+        
+        when frns_16.function = 'Wireless' AND type_of_product = 'Microwave'
+         then 'Fixed Wireless'
+        
+        when frns_16.function = 'Copper' AND type_of_product = 'Cable Modem'
+         then 'Cable'
+                     
+        when frns_16.function = 'Copper' AND type_of_product = 'Digital Subscriber Line (DSL)'
+         then 'DSL'
+                     
+        when frns_16.function = 'Copper' AND type_of_product = 'T-1'
+         then 'T-1'
+                     
+        when frns_16.function = 'Copper' AND type_of_product not in ('Cable Modem',
+                                                        'Digital Subscriber Line (DSL)',
+                                                        'T-1')
+         then 'Other Copper'
+                     
+        when frns_16.function = 'Wireless' AND type_of_product in ('Satellite Service',
+                                                      'Wireless data service',
+                                                      'Data plan for portable device')
+         then 'Satellite/LTE'
+                     
+        when li.isp_conditions_met = true
+         then 'ISP Only'
+        
+        when frns_16.function is null then 'Not Broadband'
+        
+        else
+         'Uncategorized'
+  end as connect_category,
+  li.id,
+  li.applicant_ben,
+  case when li.isp_conditions_met then 'ISP'
+	when li.upstream_conditions_met then 'Upstream'
+	when li.internet_conditions_met then 'Internet'
+	when li.wan_conditions_met then 'WAN'
+	when li.backbone_conditions_met then 'Backbone'
+	end as purpose_adj,
+	li.num_lines,
+  sr.applicant_name,
+  fr.applicant_state,
+  fr.applicant_zip_code,
+  fr.ben_applicant_type,
+  fr.ben_urban_rural_status,
+  case when sr.reporting_name is null or reporting_name = '' then sr.service_provider_name else sr.reporting_name end as service_provider,
+  dd.name,
+  dd.district_size,
+  dd.locale,
+  dd.fiber_target_status,
+  dd.discount_rate_c1_matrix,
+  recipient_include_in_universe_of_districts
+  
+  
+from public.funding_requests_2016_and_later fr
+
+left join bi_2016 bi16
+on fr.application_number = bi16.application_number
+
+left join frns_16
+on fr.frn = frns_16.frn
+
+left join public.esh_line_items li
+on frns_16.line_item = li.frn_complete
+
+left join entity_bens eba
+  on li.applicant_ben = eba.ben
+
+left join public.fy2016_district_lookup_matr dl
+on eba.entity_id::varchar = dl.esh_id::varchar
+
+left join public.fy2016_districts_deluxe_matr dd
+on dl.esh_id = dd.esh_id
+
+left join public.fy2016_services_received_matr sr
+on li.id = sr.irt_line_item_id
+
+where  bi16.category_of_service::numeric = 1
+	and fr.frn_status='Denied'
+	order by applicant_name),
+	
+frns_15 as (
+select 
+  c."Application Number" as application_number,
+  c."FRN" as frn,
+  concat(c."FRN", '-', c."FRN Line Item No") as frn_complete,
+  c."Serv Type" as service_type,
+  c."Serv/Conn Subtype" as type_of_product,
+  null as function,
+  c."Purpose" as purpose,
+  c."IC/BMIC Make" as make,
+  c."IC/BMIC Model" as model,
+  null as other_manufacture,
+  null as basic_firewall_protection,
+  null as is_installation_included_in_price,
+  c."Unit" as unit,
+  c."C1 # of Lines" as monthly_quantity,
+  null as one_time_quantity,
+  c."Rec Elig Cost" as total_monthly_eligible_recurring_costs,
+  c."Total Cost" - c."One-time Elig Cost" as total_eligible_recurring_costs,
+  c."One-time Elig Cost" as total_eligible_one_time_costs,
+  c."Lease/Purch" as lease_or_non_purchase_agreement,
+  f."Serv Descrip" as narrative
+from public.fy2015_item21_services_and_costs c
+left join public.fy2015_funding_request_key_informations f
+on c."FRN" = f."FRN"
+where c."FRN" not in (
+  select distinct "FRN"
+  from fy2015.current_funding_request_key_informations
+)
+union
+select 
+  c."Application Number" as application_number,
+  c."FRN" as frn,
+  concat(c."FRN", '-', c."FRN Line Item No") as frn_complete,
+  c."FRN Service Type" as service_type,
+  c."Serv/Conn Subtype" as type_of_product,
+  null as function,
+  c."Purpose" as purpose,
+  c."IC/BMIC Make" as make,
+  c."IC/BMIC Model" as model,
+  null as other_manufacture,
+  null as basic_firewall_protection,
+  null as is_installation_included_in_price,
+  c."Unit" as unit,
+  c."C1 # of Lines" as monthly_quantity,
+  null as one_time_quantity,
+  c."Rec Elig Cost" as total_monthly_eligible_recurring_costs,
+  c."Total Cost" - c."One-time Elig Cost" as total_eligible_recurring_costs,
+  c."One-time Elig Cost" as total_eligible_one_time_costs,
+  c."Lease/Purch" as lease_or_non_purchase_agreement,
+  f."Serv Descrip" as narrative
+  
+from fy2015.current_item21_services_and_costs c
+left join fy2015.current_funding_request_key_informations f
+on c."FRN" = f."FRN"),
+
+final_2015 as (
+
+select distinct
+  frns_15.frn,
+  frns_15.total_monthly_eligible_recurring_costs,
+  frns_15.total_eligible_one_time_costs,
+  li.id,
+  li.applicant_ben,
+  case when li.isp_conditions_met then 'ISP'
+	when li.upstream_conditions_met then 'Upstream'
+	when li.internet_conditions_met then 'Internet'
+	when li.wan_conditions_met then 'WAN'
+	when li.backbone_conditions_met then 'Backbone'
+	end as purpose_adj,
+	li.num_lines
+
+from frns_15
+
+left join
+public.esh_line_items li
+on frns_15.frn_complete = li.frn_complete
+
+left join 
+public.line_items pli
+
+on li.id = pli.id
+
+where li.broadband=true
+and li.funding_year=2015
+and (not('exclude'=any(pli.open_flags)) or pli.open_flags is null)
+),
+
+final_w_matches_2016 as (
+select distinct final_2016.*,
+
+case when final_2016.purpose_adj = final_2015.purpose_adj
+and final_2016.applicant_ben = final_2015.applicant_ben
+and final_2016.num_lines = final_2015.num_lines
+then 1 else 0 end as match,
+
+case when final_2016.purpose_adj = final_2015.purpose_adj
+and final_2016.applicant_ben = final_2015.applicant_ben
+and final_2016.num_lines = final_2015.num_lines
+then final_2015.total_monthly_eligible_recurring_costs 
+end as total_monthly_eligible_recurring_costs_15,
+
+case when final_2016.purpose_adj = final_2015.purpose_adj
+and final_2016.applicant_ben = final_2015.applicant_ben
+and final_2016.num_lines = final_2015.num_lines
+then final_2015.total_eligible_one_time_costs 
+end as total_eligible_one_time_costs_15
+
+from final_2016
+left join final_2015 
+on final_2016.purpose_adj = final_2015.purpose_adj
+and final_2016.applicant_ben = final_2015.applicant_ben
+and final_2016.num_lines = final_2015.num_lines
+)
+
+select f.* from final_w_matches_2016 f
+join public.tags t
+on f.id=t.taggable_id
+where fiber_sub_type = 'Special Construction'
+and t.label = 'special_construction_tag'
+--Desoto
+--and application_number = '161012491'
+	
+
+	
---
+++ Projects/erate_defense/src/q2/mungedata_fcdl.py
@@ -0,0 +1,132 @@
+##imports and definitions
+#import packages
+import pandas as pd
+import numpy as np
+import re
+import string
+from collections import Counter
+from nltk.corpus import stopwords, PlaintextCorpusReader
+from itertools import tee, islice
+
+#import environment variables
+import os
+GITHUB = os.environ.get("GITHUB")
+
+#define classes
+punct = re.compile('[%s]' % re.escape(string.punctuation))
+def punct_re(s):
+    return punct.sub(' ', s)
+
+stops = stopwords.words("english")
+def remove_stops(text):
+    return [item for item in text if item not in stops]
+
+#adapeted from https://github.com/educationsuperhighway/ficher/blob/master/Projects/streamlined_app/src/fcdl.py
+def ngrams(lst, n):
+  tlst = lst
+  while True:
+    a, b = tee(tlst)
+    l = tuple(islice(a, n))
+    if len(l) == n:
+      yield l
+      next(b)
+      tlst = b
+    else:
+      break
+
+#import data
+os.chdir(GITHUB+'/Projects/erate_defense/data/') 
+frns_2016 = pd.read_csv('frns_2016.csv', encoding = "ISO-8859-1")
+frns_2017 = pd.read_csv('frns_2017.csv', encoding = "ISO-8859-1")
+
+
+
+#filter to those frns where the FRN doesn't have a pending or cancelled status
+frns_2016_model  = frns_2016.loc[np.logical_or(frns_2016['funded_frn']> 0, frns_2016['denied_frn']>0)]
+frns_2017_model  = frns_2017.loc[np.logical_or(frns_2017['funded_frn']> 0, frns_2017['denied_frn']>0)]
+
+
+#reset index
+frns_model = frns_model.reset_index(drop=True)
+
+##prep frns data for narrative analysis
+#adapted from https://github.com/educationsuperhighway/ficher/blob/master/Projects/streamlined_app/src/evan_fcdl.py
+#only determine denial category for those that were originally denied
+frns_denied = frns_model.loc[frns_model.orig_denied_frn == 1].copy()
+
+#reset index
+frns_denied = frns_denied.reset_index(drop = True)
+
+#making the narrative lowercase
+fcdl_comment_cleaned = frns_denied['fcdl_comment_for_frn'].str.lower()
+
+#removing trailing and leading whitespaces
+fcdl_comment_cleaned = fcdl_comment_cleaned.str.strip()
+
+#removing punctuation
+fcdl_comment_cleaned = fcdl_comment_cleaned.apply(lambda x: punct_re(str(x)))
+
+#creating new column in dataframe that is the cleaned up text
+frns_denied['fcdl_comment_cleaned'] = fcdl_comment_cleaned
+
+#creating a new columns array of all the words
+frns_denied['fcdl_comment_array'] = frns_denied['fcdl_comment_cleaned'].apply(lambda x: x.split())
+
+#removing stop words
+frns_denied['fcdl_comment_array'] = frns_denied['fcdl_comment_array'].apply(lambda x: remove_stops(x))
+
+#re-creating the cleaned column after removing stop words
+frns_denied['fcdl_comment_cleaned'] = frns_denied['fcdl_comment_array'].apply(lambda x: ' '.join(x))
+
+
+##prep frns for trigrams categorization
+#note: code takes 30 sec - 1 min
+
+#create counter array of all trigrams in narrative
+frns_denied['trigram_array'] = frns_denied['fcdl_comment_array'].apply(lambda x: Counter(ngrams(x, 3)))
+
+#create empty array to fill with all categorized FRNs
+frn_cats = pd.DataFrame(columns=['CATEGORY', 'frn'])
+
+#create range for which to loop through FRNS
+index_frn_cats = range(0,len(frns_denied))
+
+#for each FRN, create a dataframe of the possible categories
+for j in index_frn_cats:
+
+  #create empty array which will serve as a placeholder for each FRNs categories
+  cats = []
+
+  #create empty array which will serve as a placeholder for each FRNs categories
+  index_cats = range(0,len(trigrams_cats))
+
+  #for each category, convert trigram to tuple in order to check if that trigram is in the narrative
+  for i in index_cats:
+    cats.append(tuple(re.findall(r"[\w]+", trigrams_cats['trigram'][i])) in frns_denied['trigram_array'][j])
+
+  #convert to data frame
+  cats = pd.DataFrame(data=cats, index=index_cats, columns=['in_array'])
+  
+  #add the category for each trigram for reference
+  cats = pd.concat([cats, trigrams_cats[['CATEGORY']]], axis=1)
+  
+  #add the FRN for each loop for reference
+  cats['frn'] = frns_denied['frn'][j]
+  
+  #filter to categories that are in the array
+  cats = cats.loc[cats.in_array == True]
+  
+  #filter to columns we want to record
+  cats = cats[['CATEGORY', 'frn']]
+  
+  #drop duplicated categories
+  cats = cats.drop_duplicates()
+  
+  #append daraframe to all other FRNs recorded
+  frn_cats = pd.concat([frn_cats, cats], axis=0)
+
+#reset index of final dataframe
+frn_cats = frn_cats.reset_index(drop = True)
+
+##merge trigrams categorizations with frns data
+#create indicator for each category
